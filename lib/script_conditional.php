66be7adf1603737a341179ec18b9f01d9f3c96b0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-02-08 11:49:17 -0200

    Remoção de código não utilizado.

--- lib/script_conditional.php
+++ lib/script_conditional.php
@@ -10,11 +10,3 @@ function wp_script_add_data( $handle, $key, $value ) {
 }
 
 endif;
-
-// add_filter( 'script_loader_tag', function( $tag, $handle ) {
-//     global $wp_scripts;
-//     if( isset( $wp_scripts->registered[$handle]->extra['conditional'] ) && $wp_scripts->registered[$handle]->extra['conditional'] ) {
-//         $tag = "<!--[if {$wp_scripts->registered[$handle]->extra['conditional']}]>\n" . $tag . "<![endif]-->\n";
-//     }
-//     return $tag;
-// }, 10, 2 );
