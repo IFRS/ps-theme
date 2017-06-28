591224cb15b8d3d88f78ed97122058ef45a7e95b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-28 15:41:17 -0300

    Melhorias nos hooks para criar e remover as roles.

--- lib/permissions.php
+++ lib/permissions.php
@@ -4,39 +4,48 @@ add_action('init', function() {
     global $wp_post_types;
     $wp_post_types['attachment']->cap->edit_posts = 'manage_files';
     $wp_post_types['attachment']->cap->delete_posts = 'manage_files';
+});
 
+add_action('after_switch_theme', function() {
     // Cursos Role
+    if (!get_role( 'cadastrador_cursos' )) {
+        add_role('cadastrador_cursos', __('Cadastrador de Cursos'), array(
+            'read'              => true,
+            'upload_files'      => true,
+            'manage_files'      => true,
+
+            'create_cursos'     => true,
+            'edit_cursos'       => true,
+            'manage_cursos'     => false,
+
+            'assign_campus'     => true,
+            'assign_turno'      => true,
+            'assign_modalidade' => true
+        ));
+    }
+
+    // Resultados Role
+    if (!get_role( 'cadastrador_resultados' )) {
+        add_role('cadastrador_resultados', __('Cadastrador de Resultados'), array(
+            'read'                 => true,
+            'upload_files'         => true,
+            'manage_files'         => true,
+
+            'create_resultados'    => true,
+            'edit_resultados'      => true,
+            'manage_resultados'    => false,
+
+            'assign_campus'        => true,
+            'assign_formaingresso' => true
+        ));
+    }
+});
+
+add_action('switch_theme', function() {
     if (get_role( 'cadastrador_cursos' )) {
         remove_role( 'cadastrador_cursos' );
     }
-    add_role('cadastrador_cursos', __('Cadastrador de Cursos'), array(
-        'read'              => true,
-        'upload_files'      => true,
-        'manage_files'      => true,
-
-        'create_cursos'     => true,
-        'edit_cursos'       => true,
-        'manage_cursos'     => false,
-
-        'assign_campus'     => true,
-        'assign_turno'      => true,
-        'assign_modalidade' => true
-    ));
-
-    // Resultados Role
     if (get_role( 'cadastrador_resultados' )) {
         remove_role( 'cadastrador_resultados' );
     }
-    add_role('cadastrador_resultados', __('Cadastrador de Resultados'), array(
-        'read'                 => true,
-        'upload_files'         => true,
-        'manage_files'         => true,
-
-        'create_resultados'    => true,
-        'edit_resultados'      => true,
-        'manage_resultados'    => false,
-
-        'assign_campus'        => true,
-        'assign_formaingresso' => true
-    ));
 });
