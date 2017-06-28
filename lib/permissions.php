8867686cf1862b7d36ec6aeb03390d066e2d0f3b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-06-28 10:16:15 -0300

    Criação e adição de permissões para gerenciar mídias.

--- lib/permissions.php
+++ lib/permissions.php
@@ -1,5 +1,10 @@
 <?php
 add_action('init', function() {
+    // Fix Media Permissions
+    global $wp_post_types;
+    $wp_post_types['attachment']->cap->edit_posts = 'manage_files';
+    $wp_post_types['attachment']->cap->delete_posts = 'manage_files';
+
     // Cursos Role
     if (get_role( 'cadastrador_cursos' )) {
         remove_role( 'cadastrador_cursos' );
@@ -7,6 +12,7 @@ add_action('init', function() {
     add_role('cadastrador_cursos', __('Cadastrador de Cursos'), array(
         'read'              => true,
         'upload_files'      => true,
+        'manage_files'      => true,
 
         'create_cursos'     => true,
         'edit_cursos'       => true,
@@ -24,6 +30,7 @@ add_action('init', function() {
     add_role('cadastrador_resultados', __('Cadastrador de Resultados'), array(
         'read'                 => true,
         'upload_files'         => true,
+        'manage_files'         => true,
 
         'create_resultados'    => true,
         'edit_resultados'      => true,
