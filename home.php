e8a2571f3f699c18c53b770ae04d1199e08f6f39
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-09-15 18:06:24 -0300

    Readequação do template home para mostrar somente notícias.

--- home.php
+++ home.php
@@ -1,15 +1,7 @@
 <?php get_header(); ?>
 
 <section class="container" id="home-content">
-    <?php echo get_template_part('partials/banners'); ?>
-
-    <div id="ms-grid" class="row">
-        <div class="row">
-            <div class="col-xs-12 col-md-6 ms-item">
-                <?php if (!dynamic_sidebar('home_1')) : endif; ?>
-            </div>
-        </div>
-
+    <div class="row">
         <?php echo get_template_part('partials/loop-avisos'); ?>
     </div>
 </section>
