aea95e5bedcc53125036f68c73f9e55de20165f6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-09-19 10:17:30 -0300

    Adição do título da página com todas as notícias.

--- home.php
+++ home.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
 
 <section class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <h2><?php echo get_the_title(get_option( 'page_for_posts' )); ?></h2>
+        </div>
+    </div>
     <?php echo get_template_part('partials/loop-avisos'); ?>
 </section>
 
