a8a88a19ff28218c0d59bd72ff12b5914f50357f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-09-19 11:01:52 -0300

    Alinhamento nas configurações para os Widgets.

--- lib/widgets/widgets.php
+++ lib/widgets/widgets.php
@@ -1,18 +1,18 @@
 <?php
 function ps_widgets_init() {
 	register_sidebar(array(
-		'name' => 'Home Atalhos',
-		'id' => 'home_atalhos',
-		'description' => 'Atalhos na página inicial.',
+		'name'          => 'Home Atalhos',
+		'id'            => 'home_atalhos',
+		'description'   => 'Atalhos na página inicial.',
 		'before_widget' => '<div id="%1$s" class="col-xs-12 col-sm-6 col-md-3 widget widget-atalhos %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<span class="sr-only">',
 		'after_title'   => '</span>',
 	));
 	register_sidebar(array(
-		'name' => 'Home Jumbo',
-		'id' => 'home_jumbo',
-		'description' => 'Widgets grandes na página inicial.',
+		'name'          => 'Home Jumbo',
+		'id'            => 'home_jumbo',
+		'description'   => 'Widgets grandes na página inicial.',
 		'before_widget' => '<div id="%1$s" class="col-xs-12 widget widget-jumbo %2$s">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="widget-jumbo__title">',
