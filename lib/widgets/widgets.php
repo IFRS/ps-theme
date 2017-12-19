bffb392bcc80ded1e19085baaaf1dbe205520daf
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-12-19 13:23:39 -0200

    Estilização do widget de chamadas.

--- lib/widgets/widgets.php
+++ lib/widgets/widgets.php
@@ -6,8 +6,8 @@ function ps_widgets_init() {
 		'description'   => 'Chamadas na página inicial.',
 		'before_widget' => '<div id="%1$s" class="col-xs-12 col-md-6 col-md-offset-5 col-lg-6 col-lg-offset-5 widget widget-chamadas %2$s">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<div class="widget_chamadas_widget__title"><h2>',
-		'after_title'   => '</h2></div>',
+		'before_title'  => '<div class="widget_chamadas_widget__title"><h2><strong>',
+		'after_title'   => '</strong></h2></div>',
 	));
 	register_sidebar(array(
 		'name'          => 'Home Atalhos',
