986e6c24a3d3227f9f629bc510cc48a4b645eaca
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2017-12-19 10:38:00 -0200

    Renomeação do script para controlar o widget de chamadas.

--- src/chamadas.js
+++ src/chamadas.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $('#resultados').find('div.campi, div.modalidades, div.resultados').hide();
+    $('#chamadas').find('div.campi, div.modalidades, div.chamadas').hide();
 
-    $('#resultados a.toggle').on('click', function(e) {
+    $('#chamadas a.toggle').on('click', function(e) {
         var link = $(this);
 
         $(link.attr('href')).parent().children().fadeOut(500).promise().done(function() {
-            $(link.attr('href')).children().not('div.campi, div.modalidades, div.resultados').show();
+            $(link.attr('href')).children().not('div.campi, div.modalidades, div.chamadas').show();
             $(link.attr('href')).fadeIn(500, function() {
-                $('#resultados').resize();
+                $('#chamadas').resize();
                 if ( $.isFunction($.fn.masonry) ) {
                     $('#ms-grid').masonry();
                 }
@@ -17,13 +17,13 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
-    $('#resultados .breadcrumb a').on('click', function(e) {
+    $('#chamadas .breadcrumb a').on('click', function(e) {
         var link = $(this);
 
         $(link.attr('href')).children().fadeOut(500).promise().done(function() {
-            link.closest('div.campi, div.modalidades, div.resultados').children().hide();
-            $(link.attr('href')).children().not('div.campi, div.modalidades, div.resultados').fadeIn(500, function() {
-                $('#resultados').resize();
+            link.closest('div.campi, div.modalidades, div.chamadas').children().hide();
+            $(link.attr('href')).children().not('div.campi, div.modalidades, div.chamadas').fadeIn(500, function() {
+                $('#chamadas').resize();
                 if ( $.isFunction($.fn.masonry) ) {
                     $('#ms-grid').masonry();
                 }
