1be34fb3a7eddfb1b4deb98b1f2f0fb582757ebc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-06 14:15:55 -0200

    Ajustes em URLs e textos do rodapé.

--- footer.php
+++ footer.php
@@ -30,13 +30,13 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 col-md-3">
-                <a href="//ifrs.edu.br/" title="Portal do IFRS" class="footer-logo"><img class="center-block img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ifrs-branco.png" alt="Marca do IFRS"/></a>
+                <a href="http://ifrs.edu.br/" title="Portal do IFRS" class="footer-logo"><img class="center-block img-responsive" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ifrs-branco.png" alt="Marca do IFRS"/></a>
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6">
                 <address class="footer-address">
                     <p>Rua General Os&oacute;rio, 348 | Bairro Centro</p>
-                    <p>CEP: 95700-086 | Bento Gonçalves/RS</p>
-                    <p>Email: <a href="mailto:pssolicitacoes@ifrs.edu.br">pssolicitacoes@ifrs.edu.br</a></p>
+                    <p>CEP: 95700-086 | Bento Gon&ccedil;alves/RS</p>
+                    <p>E-mail: <a href="mailto:pssolicitacoes@ifrs.edu.br">pssolicitacoes@ifrs.edu.br</a></p>
                     <p>Telefone: (54) 3449-3300</p>
                 </address>
             </div>
@@ -65,10 +65,10 @@
             <div class="col-xs-12">
                 <p>
                     <!-- Wordpress -->
-                    <a href="http://www.wordpress.org/" target="blank">Desenvolvido com Wordpress<span class="sr-only"> (abre uma nova p&aacute;gina)</span></a> <span class="glyphicon glyphicon-new-window"></span>
+                    <a href="http://br.wordpress.org/" target="blank">Desenvolvido com Wordpress<span class="sr-only"> (abre uma nova p&aacute;gina)</span></a> <span class="glyphicon glyphicon-new-window"></span>
                     &mdash;
                     <!-- Código-fonte GPL -->
-                    <a href="https://github.com/IFRS/ps-theme" target="blank">C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3<span class="sr-only"> (abre uma nova p&aacute;gina)</span></a> <span class="glyphicon glyphicon-new-window"></span>
+                    <a href="https://github.com/IFRS/ps-theme/" target="blank">C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3<span class="sr-only"> (abre uma nova p&aacute;gina)</span></a> <span class="glyphicon glyphicon-new-window"></span>
                     &mdash;
                     <!-- Creative Commons -->
                     <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="blank"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/cc-by-nc-nd.png" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-SemDeriva&ccedil;&otilde;es 4.0 Internacional (abre uma nova p&aacute;gina)" /></a> <span class="glyphicon glyphicon-new-window"></span>
