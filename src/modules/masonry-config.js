daf13487b119729ee0cf5899c42f8c5d3ef1ccee
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-06 14:25:28 -0200

    Remoção do módulo do Masonry.

--- src/modules/masonry-config.js
+++
@@ -1,9 +0,0 @@
-require('jquery-bridget');
-require('masonry-layout');
-
-$(window).load(function() {
-    $('#ms-grid').masonry({
-        itemSelector: '.ms-item',
-        percentPosition: true
-    });
-});
--- src/ps.js
+++ src/ps.js
@@ -5,4 +5,3 @@ require('./modules/chamadas');
 require('./modules/cursos-tab-from-url');
 require('./modules/dataTables-pt_BR');
 require('./modules/fancybox-config');
-require('./modules/masonry-config');
