e3265f220404dea222e0956133a8a6bfbaf5ca64
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-09 11:15:17 -0200

    Adaptação de formulário.

--- searchform.php
+++ searchform.php
@@ -2,8 +2,8 @@
     <label class="sr-only" for="search-field">Buscar por:</label>
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="search-field form-control" placeholder="Buscar em todo o site" required>
-        <span class="input-group-btn">
-            <button type="submit" class="search-submit btn btn-default" title="Buscar"><span class="glyphicon glyphicon-search"></span></button>
+        <span class="input-group-append">
+            <button type="submit" class="search-submit btn btn-secondary" title="Buscar"><span class="glyphicon glyphicon-search"></span></button>
         </span>
     </div>
 </form>
