6e4e89d8c0dc1f6d7e4120663127cce779e3a929
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-20 11:12:03 -0300

    Ajuste do texto no botão de busca para um símbolo de seta.

--- searchform.php
+++ searchform.php
@@ -3,7 +3,7 @@
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="search-field form-control form-control-sm" placeholder="Buscar em todo o site" required>
         <span class="input-group-append">
-            <button type="submit" class="search-submit btn btn-primary btn-sm" title="Buscar">Ir</button>
+            <button type="submit" class="search-submit btn btn-primary btn-sm" title="Buscar">&rarrhk;</button>
         </span>
     </div>
 </form>
