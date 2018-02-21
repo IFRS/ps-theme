be48ff7d02ed120adc74cc3ff626cf4e66d1a5f7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-21 17:31:23 -0300

    Remoção de classe não utilizadas.

--- searchform.php
+++ searchform.php
@@ -1,9 +1,9 @@
-<form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url(home_url('/')); ?>">
+<form role="search" method="get" class="form-inline" action="<?php echo esc_url(home_url('/')); ?>">
     <label class="sr-only" for="search-field">Buscar por:</label>
     <div class="input-group">
         <input type="search" value="<?php echo get_search_query(); ?>" name="s" id="search-field" class="search-field form-control form-control-sm" placeholder="Buscar em todo o site" required>
         <span class="input-group-append">
-            <button type="submit" class="search-submit btn btn-primary btn-sm" title="Buscar">&rarrhk;</button>
+            <button type="submit" class="btn btn-info btn-sm" title="Buscar">&rarrhk;</button>
         </span>
     </div>
 </form>
