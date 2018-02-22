8273849fc8ffbc555a027e29d84f5863e5d3052e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-22 17:27:57 -0300

    Ajuste na exibição da lista de Editais.

--- partials/edital-item.php
+++ partials/edital-item.php
@@ -1,9 +1,10 @@
 <?php $parent = wp_get_post_parent_id(get_the_ID()); ?>
-<a href="<?php echo get_post_meta(get_the_ID(), '_edital_arquivo', true); ?>" rel="bookmark" class="list-group-item<?php echo (empty($parent) ? ' active' : ' child'); ?>" target="_blank" title="<?php the_title(); ?> (abre em uma nova p&aacute;gina)">
-    <h4 class="list-group-item-heading"><?php the_title(); ?><span class="sr-only">&nbsp;(abre uma nova p&aacute;gina)</span><span class="glyphicon glyphicon-new-window pull-right"></span></h4>
-    <p class="list-group-item-text">
-        <small>
-            <span class="glyphicon glyphicon-calendar"></span>&nbsp;<?php if (get_the_modified_time() != get_the_time()) : ?>atualizado em <?php the_modified_time('d'); ?> de <?php the_modified_time('F'); ?> de <?php the_modified_time('Y'); ?>&nbsp; | &nbsp;<?php endif; ?>publicado em <?php the_time('d'); ?> de <?php the_time('F'); ?> de <?php the_time('Y'); ?>
-        </small>
+<a href="<?php echo get_post_meta(get_the_ID(), '_edital_arquivo', true); ?>" rel="bookmark" class="flex-column align-items-start list-group-item list-group-item-action<?php echo (empty($parent) ? ' list-group-item-primary' : ' list-group-item-light child'); ?>" target="_blank" title="<?php the_title(); ?> (abre em uma nova p&aacute;gina)">
+    <div class="d-flex w-100 justify-content-between">
+        <h4 class="mb-1"><?php the_title(); ?><span class="sr-only">&nbsp;(abre uma nova p&aacute;gina)</span></h4>
+        <small></small>
+    </div>
+    <p class="mb-1">
+        <small><?php if (get_the_modified_time() != get_the_time()) : ?>atualizado em <?php the_modified_time('d'); ?> de <?php the_modified_time('F'); ?> de <?php the_modified_time('Y'); ?>&nbsp; | &nbsp;<?php endif; ?>publicado em <?php the_time('d'); ?> de <?php the_time('F'); ?> de <?php the_time('Y'); ?></small>
     </p>
 </a>
