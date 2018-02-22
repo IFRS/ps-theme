56c3b2a44135d26bcb31d68f61f267d7ec382ae3
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-22 16:44:42 -0300

    Reorganização e estilização das Publicações.

--- single-publicacao.php
+++ single-publicacao.php
@@ -11,34 +11,31 @@
                         <h2 class="publicacao__title"><?php the_title(); ?></h2>
                     </div>
                 </div>
-                <div class="row publicacao__meta">
-                    <div class="col-12 col-md-6">
-                        <p class="publicacao__date">Publicado em <?php the_date('d/m/Y'); ?></p>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <p class="publicacao__date">Atualizado em <?php the_modified_date('d/m/Y'); ?></p>
-                    </div>
-                </div>
+
                 <div class="row">
                     <div class="col-12">
                         <div class="publicacao__content">
+                            <div class="row publicacao__meta">
+                                <div class="col-12 col-md-6">
+                                    <p class="publicacao__date text-left">Publicado em <?php the_date('d/m/Y'); ?></p>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <p class="publicacao__date text-right">Atualizado em <?php the_modified_date('d/m/Y'); ?></p>
+                                </div>
+                            </div>
                             <?php the_content(); ?>
+                            <?php $files = get_post_meta( get_the_ID(), '_publicacao_arquivos', true ); ?>
+                            <?php if (!empty($files)) : ?>
+                                <h3>Arquivos</h3>
+                                <div class="list-group">
+                                <?php foreach ($files as $id => $file) : ?>
+                                    <a class="list-group-item" href="<?php echo esc_url($file); ?>"><span class="glyphicon glyphicon-save-file"></span>&nbsp;<?php echo get_the_title($id); ?></a>
+                                <?php endforeach; ?>
+                                </div>
+                            <?php endif; ?>
                         </div>
                     </div>
                 </div>
-                <?php $files = get_post_meta( get_the_ID(), '_publicacao_arquivos', true ); ?>
-                <?php if (!empty($files)) : ?>
-                    <div class="row">
-                        <div class="col-12">
-                            <h3>Arquivos</h3>
-                            <div class="list-group">
-                            <?php foreach ($files as $id => $file) : ?>
-                                <a class="list-group-item" href="<?php echo esc_url($file); ?>"><span class="glyphicon glyphicon-save-file"></span>&nbsp;<?php echo get_the_title($id); ?></a>
-                            <?php endforeach; ?>
-                            </div>
-                        </div>
-                    </div>
-                <?php endif; ?>
             </article>
         </div>
         <div class="col-12 col-lg-4">
@@ -68,19 +65,15 @@
                             $cat_posts = get_posts($args);
                         ?>
                         <?php if (!empty($cat_posts)) : ?>
-                            <h3>Outras Publica&ccedil;&otilde;es</h3>
+                            <h3 class="aside__title">Outras Publica&ccedil;&otilde;es</h3>
                             <?php foreach ($cat_posts as $cat_post) : ?>
-                                <div class="media">
-                                    <div class="media-left media-middle">
-                                        <?php if (has_post_thumbnail($cat_post->ID)) : ?>
-                                            <a href="<?php echo get_permalink($cat_post->ID); ?>">
-                                                <?php echo get_the_post_thumbnail($cat_post->ID, 'thumbnail', array('class' => 'media-object')); ?>
-                                            </a>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="media-body">
-                                        <h4 class="media-heading"><a href="<?php echo get_permalink($cat_post->ID); ?>" rel="bookmark"><?php echo $cat_post->post_title; ?></a></h4>
-                                        <p><?php echo get_the_date('d/m/Y', $cat_post->ID); ?></p>
+                                <div class="card">
+                                    <?php if (has_post_thumbnail($cat_post->ID)) : ?>
+                                        <?php echo get_the_post_thumbnail($cat_post->ID, 'thumbnail', array('class' => 'card-img-top')); ?>
+                                    <?php endif; ?>
+                                    <div class="card-body">
+                                        <h4 class="card-title"><a href="<?php echo get_permalink($cat_post->ID); ?>" rel="bookmark"><?php echo $cat_post->post_title; ?></a></h4>
+                                        <p class="card-subtitle"><?php echo get_the_date('d/m/Y', $cat_post->ID); ?></p>
                                     </div>
                                 </div>
                             <?php endforeach; ?>
