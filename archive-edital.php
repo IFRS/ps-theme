a04271067609011abcb10a16d8ea0a8dbe5bc36a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-22 17:32:57 -0300

    Remoção do "corpo" para a lista de Editais.

--- archive-edital.php
+++ archive-edital.php
@@ -5,9 +5,7 @@
         <div class="col-12">
             <div class="editais">
                 <h2 class="editais__title">Editais</h2>
-                <div class="editais__content">
-                    <?php echo get_template_part('partials/loop', 'editais'); ?>
-                </div>
+                <?php echo get_template_part('partials/loop', 'editais'); ?>
             </div>
         </div>
     </div>
