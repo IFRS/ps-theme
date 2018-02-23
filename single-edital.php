51e04ab784b82034e5464419b751a4fa32e9d576
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-02-23 09:04:47 -0300

    Reorganização e estilização do single edital.

--- single-edital.php
+++ single-edital.php
@@ -11,24 +11,24 @@
                         <h2 class="edital__title"><?php the_title(); ?></h2>
                     </div>
                 </div>
-                <div class="row edital__meta">
-                    <div class="col-12 col-md-6">
-                        <p class="edital__date">Publicado em <?php the_date('d/m/Y'); ?></p>
-                    </div>
-                    <div class="col-12 col-md-6">
-                        <p class="edital__date">Atualizado em <?php the_modified_date('d/m/Y'); ?></p>
-                    </div>
-                </div>
                 <div class="row">
                     <div class="col-12">
                         <div class="edital__content">
+                            <div class="row edital__meta">
+                                <div class="col-12 col-md-6">
+                                    <p class="edital__date text-left">Publicado em <?php the_date('d/m/Y'); ?></p>
+                                </div>
+                                <div class="col-12 col-md-6">
+                                    <p class="edital__date text-right">Atualizado em <?php the_modified_date('d/m/Y'); ?></p>
+                                </div>
+                            </div>
                             <?php the_content(); ?>
+                            <div class="row">
+                                <div class="col-12">
+                                    <a href="<?php echo get_post_meta(get_the_ID(), '_edital_arquivo', true); ?>" class="btn btn-primary">Baixar Arquivo</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <a href="<?php echo get_post_meta(get_the_ID(), '_edital_arquivo', true); ?>" class="btn btn-primary">Baixar Arquivo</a>
                     </div>
                 </div>
             </article>
@@ -50,19 +50,15 @@
                             $cat_posts = get_posts($args);
                         ?>
                         <?php if (!empty($cat_posts)) : ?>
-                            <h3>Outros Editais</h3>
+                            <h3 class="aside__title">Outros Editais</h3>
                             <?php foreach ($cat_posts as $cat_post) : ?>
-                                <div class="media">
-                                    <div class="media-left media-middle">
-                                        <?php if (has_post_thumbnail($cat_post->ID)) : ?>
-                                            <a href="<?php echo get_permalink($cat_post->ID); ?>">
-                                                <?php echo get_the_post_thumbnail($cat_post->ID, 'thumbnail', array('class' => 'media-object')); ?>
-                                            </a>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="media-body">
-                                        <h4 class="media-heading"><a href="<?php echo get_permalink($cat_post->ID); ?>" rel="bookmark"><?php echo $cat_post->post_title; ?></a></h4>
-                                        <p><?php echo get_the_date('d/m/Y', $cat_post->ID); ?></p>
+                                <div class="card">
+                                    <?php if (has_post_thumbnail($cat_post->ID)) : ?>
+                                        <?php echo get_the_post_thumbnail($cat_post->ID, 'thumbnail', array('class' => 'card-img-top')); ?>
+                                    <?php endif; ?>
+                                    <div class="card-body">
+                                        <h4 class="card-title"><a href="<?php echo get_permalink($cat_post->ID); ?>" rel="bookmark"><?php echo $cat_post->post_title; ?></a></h4>
+                                        <p class="card-subtitle"><?php echo get_the_date('d/m/Y', $cat_post->ID); ?></p>
                                     </div>
                                 </div>
                             <?php endforeach; ?>
