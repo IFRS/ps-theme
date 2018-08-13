8ebcd802cbf2dc2527b70093a353e31b9a2b7242
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-13 16:23:10 -0300

    Remoção da área de do widget para jumbo na home

--- lib/widgets/widgets.php
+++ lib/widgets/widgets.php
@@ -1,15 +1,6 @@
 <?php
 function ps_widgets_init() {
 	register_sidebar(array(
-		'name'          => 'Home Chamadas',
-		'id'            => 'home_chamadas',
-		'description'   => 'Chamadas na página inicial.',
-		'before_widget' => '<div id="%1$s" class="col-12 col-lg-6 offset-lg-6 col-xl-6 offset-xl-6 widget widget-chamadas %2$s">',
-		'after_widget'  => '</div>',
-		'before_title'  => '<div class="widget-chamadas__title"><h2><strong>',
-		'after_title'   => '</strong></h2></div>',
-	));
-	register_sidebar(array(
 		'name'          => 'Home Atalhos',
 		'id'            => 'home_atalhos',
 		'description'   => 'Atalhos na página inicial.',
@@ -18,14 +9,16 @@ function ps_widgets_init() {
 		'before_title'  => '<span class="sr-only">',
 		'after_title'   => '</span>',
 	));
+
 	register_sidebar(array(
-		'name'          => 'Home Jumbo',
-		'id'            => 'home_jumbo',
-		'description'   => 'Widgets grandes na página inicial.',
-		'before_widget' => '<div id="%1$s" class="col-12 widget widget-jumbo %2$s">',
+		'name'          => 'Home Chamadas',
+		'id'            => 'home_chamadas',
+		'description'   => 'Chamadas na página inicial.',
+		'before_widget' => '<div id="%1$s" class="col-12 col-lg-6 offset-lg-6 col-xl-6 offset-xl-6 widget widget-chamadas %2$s">',
 		'after_widget'  => '</div>',
-		'before_title'  => '<h2 class="widget-jumbo__title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<div class="widget-chamadas__title"><h2><strong>',
+		'after_title'   => '</strong></h2></div>',
 	));
 }
+
 add_action( 'widgets_init', 'ps_widgets_init' );
