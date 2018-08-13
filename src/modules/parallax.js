c2ab47505e73394020602ab084316621d193713e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-13 16:24:02 -0300

    Adição de parallax na área de cursos da home

---
+++ src/modules/parallax.js
@@ -0,0 +1,12 @@
+let Rellax = require('rellax');
+
+$(document).ready(function() {
+    var rellax = new Rellax('.rellax', {
+        speed: 2,
+        center: true,
+        wrapper: null,
+        round: false,
+        vertical: true,
+        horizontal: false
+    });
+});
--- src/ps.js
+++ src/ps.js
@@ -5,3 +5,4 @@ require('./modules/chamadas');
 require('./modules/cursos-tab-from-url');
 require('./modules/dataTables-pt_BR');
 require('./modules/fancybox-config');
+require('./modules/parallax');
