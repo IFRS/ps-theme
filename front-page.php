4f7933c34b7e721c723062102b841571a4e31244
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-15 13:29:57 -0300

    Estilização inicial da área de ajuda na página inicial

--- front-page.php
+++ front-page.php
@@ -55,16 +55,16 @@
 <section class="home-ajuda">
     <div class="container">
         <div class="row">
-            <div class="col-12 col-lg-6">
+            <div class="col-12 col-lg-8 col-xl-6">
                 <?php echo get_template_part('partials/home-faq'); ?>
             </div>
-            <div class="col-12 col-lg-6 home-ajuda__duvida">
+            <div class="col-12 col-lg-4 col-xl-6 home-ajuda__duvida">
                 <h2 class="home-ajuda__title">Voc&ecirc; ainda tem alguma d&uacute;vida?</h2>
                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/home-ajuda.png" alt="Você tem alguma dúvida?" class="img-fluid home-ajuda__image"/>
                 <div class="home-ajuda__box">
                     <div class="row">
                         <div class="col-12">
-                            <h3 class="home-ajuda__box-title">Entre em contato...</h3>
+                            <h3 class="home-ajuda__box-title">Entre em contato:</h3>
                         </div>
                     </div>
                     <div class="row">
