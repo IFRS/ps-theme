b5be496c4982489294d7390f65ef39265fdac729
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-15 13:41:53 -0300

    Adição de imagem de título e link da área de cursos

--- front-page.php
+++ front-page.php
@@ -85,10 +85,12 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <img class="guria1 rellax" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria1.png" alt="" data-rellax-speed="2">
-                <img class="guria2 rellax d-none d-sm-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria2.png" alt="" data-rellax-speed="3">
-                <img class="guria3 rellax d-none d-lg-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria3.png" alt="" data-rellax-speed="2">
-                <img class="guria4 rellax d-none d-xl-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria4.png" alt="" data-rellax-speed="1">
+                <img class="home-cursos__title" src="<?php echo get_stylesheet_directory_uri(); ?>/img/cursos-title.png" alt="">
+                <img class="home-cursos__guria1 rellax" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria1.png" alt="" data-rellax-speed="2">
+                <img class="home-cursos__guria2 rellax d-none d-sm-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria2.png" alt="" data-rellax-speed="3">
+                <img class="home-cursos__guria3 rellax d-none d-lg-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria3.png" alt="" data-rellax-speed="2">
+                <img class="home-cursos__guria4 rellax d-none d-xl-block" src="<?php echo get_stylesheet_directory_uri(); ?>/img/guria4.png" alt="" data-rellax-speed="1">
+                <a href="/cursos"><img class="home-cursos__link" src="<?php echo get_stylesheet_directory_uri(); ?>/img/cursos-link.png" alt=""></a>
             </div>
         </div>
     </div>
