e540f0f893c6fa6463215ee217202985e3fa280b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-15 12:53:32 -0300

    Ajuste no tamanho da área de data das notícias

--- partials/home-noticias.php
+++ partials/home-noticias.php
@@ -22,13 +22,13 @@
                 <?php while ($noticias->have_posts()) : $noticias->the_post(); ?>
                     <li class="home-noticias__item">
                         <div class="row">
-                            <div class="col-4 col-lg-5 col-xl-4">
+                            <div class="col-3 col-md-2 col-lg-4 col-xl-3">
                                 <p class="home-noticias__item-meta">
                                     <span class="home-noticias__item-day"><?php echo get_the_date('d'); ?></span>
                                     <span class="home-noticias__item-month"><?php echo get_the_date('M'); ?></span>
                                 </p>
                             </div>
-                            <div class="col-8 col-lg-7 col-xl-8">
+                            <div class="col-9 col-md-8 col-lg-8 col-xl-9">
                                 <?php echo get_the_category_list(); ?>
                                 <h4 class="home-noticias__item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
                             </div>
