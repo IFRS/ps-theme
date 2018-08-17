bd5f1f697aa1e7c10603f69e8c98e5a54a6e719c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-17 17:17:07 -0300

    Ajuste nos resultados da busca para melhor estilização

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <section class="container">
     <?php if (is_search()) : ?>
-        <div class="row content">
+        <div class="row">
             <div class="col-12">
                 <h2 class="search__title">Resultados da busca por &quot;<?php the_search_query(); ?>&quot;</h2>
             </div>
@@ -14,11 +14,19 @@
             <p><strong>Ops!</strong> Nenhum resultado encontrado para o termo &quot;<?php the_search_query(); ?>&quot;.</p>
         </div>
     <?php else: ?>
-        <div class="row">
-            <?php while (have_posts()) : the_post(); ?>
-                <?php get_template_part('partials/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-            <?php endwhile; ?>
-        </div>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="row">
+                <div class="col-12">
+                    <article class="search__entry">
+                        <h3 class="search__entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                        <div class="search__entry-summary">
+                            <?php the_excerpt(); ?>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <hr>
+        <?php endwhile; ?>
     <?php endif; ?>
 
     <?php echo custom_pagination(); ?>
--- partials/content.php
+++
@@ -1,8 +0,0 @@
-<div class="col-12">
-    <article <?php post_class(); ?>>
-        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <div class="entry-summary">
-            <?php the_excerpt(); ?>
-        </div>
-    </article>
-</div>
