9df33c0ea4998719e0e7da50f2b63bb39233bbdd
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-17 14:15:21 -0300

    Estilização do título da página com todos os avisos

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <section class="container">
     <div class="row">
         <div class="col-12">
-            <h2><?php echo get_the_title(get_option( 'page_for_posts' )); ?></h2>
+            <h2 class="avisos__title"><?php echo get_the_title(get_option( 'page_for_posts' )); ?></h2>
         </div>
     </div>
     <?php echo get_template_part('partials/loop-avisos'); ?>
