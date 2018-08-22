dd52ff3db1d3649aab4291616be6d3da5de74923
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-22 10:57:13 -0300

    Mudança de nome da pasta "lib" para "inc"

--- functions.php
+++ functions.php
@@ -7,61 +7,61 @@ register_nav_menus(
 );
 
 // Permissions & Roles
-require_once('lib/permissions.php');
+require_once('inc/permissions.php');
 
 // Custom Header
-require_once('lib/custom-header.php');
+require_once('inc/custom-header.php');
 
 // Post Thumbnail
-require_once('lib/post-thumbnails.php');
+require_once('inc/post-thumbnails.php');
 
 // Breadcrumb
-require_once('lib/breadcrumb.php');
+require_once('inc/breadcrumb.php');
 
 // Script Condicional
-require_once('lib/script_conditional.php');
+require_once('inc/script_conditional.php');
 
 // Scripts & Styles
-require_once('lib/assets.php');
+require_once('inc/assets.php');
 
 // Menu do Bootstrap
-require_once('lib/vendor/bootstrap-navwalker.php');
+require_once('inc/vendor/bootstrap-navwalker.php');
 
 // Widgets
-require_once('lib/widgets/widgets.php');
-require_once('lib/widgets/chamadas.php');
+require_once('inc/widgets/widgets.php');
+require_once('inc/widgets/chamadas.php');
 
 // Tamanho do resumo e resumo em páginas
-require_once('lib/page-excerpt.php');
-require_once('lib/excerpt.php');
+require_once('inc/page-excerpt.php');
+require_once('inc/excerpt.php');
 
 // Adicionar PrettyPhoto automaticamente.
-require_once('lib/fancybox_rel.php');
+require_once('inc/fancybox_rel.php');
 
 // Paginação personalizada
-require_once('lib/pagination.php');
+require_once('inc/pagination.php');
 
 // Queries personalizadas em determinados templates.
-require_once('lib/custom-queries.php');
+require_once('inc/custom-queries.php');
 
 // Filtro para buscas vazias
-require_once('lib/empty-search-filter.php');
+require_once('inc/empty-search-filter.php');
 
 // Taxonomy Single Term
-require_once('lib/taxonomy-single-term/class.taxonomy-single-term.php');
+require_once('inc/taxonomy-single-term/class.taxonomy-single-term.php');
 
 // Taxonomies
-require_once('lib/taxonomies/campus-taxonomy.php');
-require_once('lib/taxonomies/modalidade-taxonomy.php');
-require_once('lib/taxonomies/turno-taxonomy.php');
-require_once('lib/taxonomies/formaingresso-taxonomy.php');
+require_once('inc/taxonomies/campus-taxonomy.php');
+require_once('inc/taxonomies/modalidade-taxonomy.php');
+require_once('inc/taxonomies/turno-taxonomy.php');
+require_once('inc/taxonomies/formaingresso-taxonomy.php');
 
 // Post Types
-require_once('lib/post-types/edital.php');
-require_once('lib/post-types/curso.php');
-require_once('lib/post-types/pergunta.php');
-require_once('lib/post-types/publicacao.php');
-require_once('lib/post-types/chamada.php');
+require_once('inc/post-types/edital.php');
+require_once('inc/post-types/curso.php');
+require_once('inc/post-types/pergunta.php');
+require_once('inc/post-types/publicacao.php');
+require_once('inc/post-types/chamada.php');
 
 // Shortcodes
-require_once('lib/shortcodes/campi.php');
+require_once('inc/shortcodes/campi.php');
