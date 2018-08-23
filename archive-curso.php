8f8f4f10de7c78eb5b15fd08c8acf573ce62df01
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-23 11:40:12 -0300

    Adição de responsividade nas tabelas de cursos

--- archive-curso.php
+++ archive-curso.php
@@ -48,35 +48,7 @@
                     <!-- Tab panes -->
                     <div class="tab-content">
                         <div class="tab-pane fade show active" id="tab-todos" role="tabpanel">
-                            <table class="table table-striped table-cursos">
-                                <thead class="thead-light">
-                                    <tr>
-                                        <th>Curso</th>
-                                        <th>C&acirc;mpus</th>
-                                        <th>Modalidade</th>
-                                        <th>Turnos</th>
-                                        <th>Vagas*</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php if (have_posts()) : ?>
-                                    <?php while ( have_posts() ) : the_post(); ?>
-                                        <?php get_template_part('partials/cursos', 'row'); ?>
-                                    <?php endwhile;?>
-                                <?php else : ?>
-                                    <?php if ( ! is_search()) : ?>
-                                        <div class="alert alert-warning" role="alert">
-                                            <p><strong>Aguarde!</strong> Em breve os cursos ser&atilde;o disponibilizados.</p>
-                                        </div>
-                                    <?php endif; ?>
-                                <?php endif; ?>
-                                </tbody>
-                            </table>
-                        </div>
-                        <?php if ( ! is_search()) : ?>
-                            <?php $terms = get_terms('campus'); ?>
-                            <?php foreach ($terms as $key => $campus) : ?>
-                            <div class="tab-pane fade" id="tab-<?php echo $campus->slug; ?>" role="tabpanel">
+                            <div class="table-responsive-md">
                                 <table class="table table-striped table-cursos">
                                     <thead class="thead-light">
                                         <tr>
@@ -88,25 +60,57 @@
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <?php
-                                            global $wp_query;
-                                            $args = array(
-                                                'post_type' => 'curso',
-                                                'orderby' => 'title',
-                                                'order' => 'ASC',
-                                                'campus' => $campus->slug,
-                                                'posts_per_page' => -1,
-                                                'nopaging' => true
-                                            );
-                                            $args = array_merge($wp_query->query_vars, $args);
-                                            $cursos_per_campus = new WP_Query($args);
-                                        ?>
-                                        <?php while ( $cursos_per_campus->have_posts() ) : $cursos_per_campus->the_post(); ?>
+                                    <?php if (have_posts()) : ?>
+                                        <?php while ( have_posts() ) : the_post(); ?>
                                             <?php get_template_part('partials/cursos', 'row'); ?>
                                         <?php endwhile;?>
-                                        <?php $cursos_per_campus->wp_reset_query(); ?>
+                                    <?php else : ?>
+                                        <?php if ( ! is_search()) : ?>
+                                            <div class="alert alert-warning" role="alert">
+                                                <p><strong>Aguarde!</strong> Em breve os cursos ser&atilde;o disponibilizados.</p>
+                                            </div>
+                                        <?php endif; ?>
+                                    <?php endif; ?>
                                     </tbody>
                                 </table>
+                            </div>
+                        </div>
+                        <?php if ( ! is_search()) : ?>
+                            <?php $terms = get_terms('campus'); ?>
+                            <?php foreach ($terms as $key => $campus) : ?>
+                            <div class="tab-pane fade" id="tab-<?php echo $campus->slug; ?>" role="tabpanel">
+                                <div class="table-responsive-md">
+                                    <table class="table table-striped table-cursos">
+                                        <thead class="thead-light">
+                                            <tr>
+                                                <th>Curso</th>
+                                                <th>C&acirc;mpus</th>
+                                                <th>Modalidade</th>
+                                                <th>Turnos</th>
+                                                <th>Vagas*</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                                global $wp_query;
+                                                $args = array(
+                                                    'post_type' => 'curso',
+                                                    'orderby' => 'title',
+                                                    'order' => 'ASC',
+                                                    'campus' => $campus->slug,
+                                                    'posts_per_page' => -1,
+                                                    'nopaging' => true
+                                                );
+                                                $args = array_merge($wp_query->query_vars, $args);
+                                                $cursos_per_campus = new WP_Query($args);
+                                            ?>
+                                            <?php while ( $cursos_per_campus->have_posts() ) : $cursos_per_campus->the_post(); ?>
+                                                <?php get_template_part('partials/cursos', 'row'); ?>
+                                            <?php endwhile;?>
+                                            <?php $cursos_per_campus->wp_reset_query(); ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                             <?php endforeach; ?>
                         <?php endif; ?>
