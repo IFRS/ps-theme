8c4026a860697fe5641aacf2a668bee2f91654ec
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-08-24 15:15:55 -0300

    Criação do template para páginas não encontradas

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-lg-8 offset-lg-2">
+            <div class="alert alert-danger" role="alert">
+                <p><strong>Ops!&nbsp;</strong>A p&aacute;gina que voc&ecirc; procura n&atilde;o foi encontrada. Tente voltar para a <a href="<?php echo esc_url(home_url()); ?>">p&aacute;gina inicial</a>.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
