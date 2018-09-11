87204adb059bb9e39c057cae65b326c13d4fef4a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-11 14:20:24 -0300

    Ajuste na Barra Brasil provisória

--- partials/barrabrasil.php
+++ partials/barrabrasil.php
@@ -1,2 +1,7 @@
-<!-- Barra do Governo -->
-<div id="barra-brasil" style="background:#7F7F7F; height: 33px;"></div>
+<!-- Barra do Governo (http://barra.governoeletronico.gov.br/) -->
+<div id="barra-brasil" class="d-block">
+	<ul id="menu-barra-temp" class="bg-secondary list-inline mb-0 pl-5">
+		<li class="list-inline-item pb-1 pr-2 border-right"><a href="http://brasil.gov.br" class="text-white">Portal do Governo Brasileiro</a></li>
+		<li class="list-inline-item pb-1"><a href="http://epwg.governoeletronico.gov.br/barra/atualize.html" class="text-white">Atualize sua Barra de Governo</a></li>
+	</ul>
+</div>
