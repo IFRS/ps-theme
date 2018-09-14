66d0f63e75c922aef5be17a8dda8bb15c95feb19
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2018-09-14 15:38:44 -0300

    Remoção de linhas em branco

--- inc/assets.php
+++ inc/assets.php
@@ -18,8 +18,6 @@ function ps_load_scripts() {
 
     wp_enqueue_script('js-ps', get_template_directory_uri().(WP_DEBUG ? '/js/ps.js' : '/js/ps.min.js'), array(), null, true);
 
-
-
     if (is_post_type_archive( 'curso' )) {
         wp_enqueue_script('js-cursos', get_template_directory_uri().(WP_DEBUG ? '/js/cursos.js' : '/js/cursos.min.js'), array(), null, true);
     }
