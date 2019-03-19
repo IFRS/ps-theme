b9b5d776e084f66398535301a1a5bb5f822130ae
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-03-19 13:30:30 -0300

    Correção no título do widgets de chamadas no template de archive

--- archive-chamada.php
+++ archive-chamada.php
@@ -3,7 +3,7 @@
 <section class="container">
     <div class="row">
         <div class="col-12">
-            <?php the_widget( 'Chamadas_Widget', array('title' => 'Chamadas do Processo Seletivo'), array('before_title' => '<div class="widget-chamadas__title"><h3>', 'after_title' => '</h3></div>') ); ?>
+            <?php the_widget( 'Chamadas_Widget', array('title' => 'Chamadas do Processo Seletivo'), array('before_title' => '<div class="widget-chamadas__title"><h2 class="sr-only">', 'after_title' => '</h2></div>') ); ?>
         </div>
     </div>
 </section>
