cdbd68ddb35d486eb8a9baac771fce6a45fa48d1
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-01 16:53:51 -0300

    Estilização e reorganização do menu principal

--- partials/menu.php
+++ partials/menu.php
@@ -1,24 +1,15 @@
 <a href="#inicio-menu" id="inicio-menu" class="sr-only">In&iacute;cio do menu</a>
 
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+<button class="btn btn-menu-toggle btn-lg d-block mx-auto d-lg-none"><span class="sr-only">Esconder/Mostrar&nbsp;</span>Menu</button>
+<?php
+    wp_nav_menu( array(
+        'theme_location'    => 'main',
+        'depth'             => 2,
+        'container'         => 'nav',
+        'container_class'   => 'menu-navbar collapse show',
+        'container_id'      => false,
+        'menu_class'        => 'menu-principal nav flex-column',
+    ));
+?>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <?php
-            wp_nav_menu( array(
-                'theme_location'    => 'main',
-                'depth'             => 2,
-                'container'         => false,
-                'container_class'   => 'collapse navbar-collapse',
-                'container_id'      => 'navbarSupportedContent',
-                'menu_class'        => 'navbar-nav mr-auto',
-                'walker'            => new Bootstrap_NavWalker(),
-                'fallback_cb'       => 'Bootstrap_NavWalker::fallback'
-            ));
-        ?>
-
-        <a href="#fim-menu" id="fim-menu" class="sr-only">Fim do menu</a>
-    </div>
-</nav>
+<a href="#fim-menu" id="fim-menu" class="sr-only">Fim do menu</a>
