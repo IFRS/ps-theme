34c7a865f9a4c694747df94a7605ffce972ab47f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-02 17:14:25 -0300

    Remoção de código não mais útil

--- src/modules/flip.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(window).on('scroll', function() {
-        clearTimeout($.data(this, 'scrollTimer'));
-        $.data(this, 'scrollTimer', setTimeout(function() {
-            $('.home-ajuda__image').toggleClass('home-ajuda__image_reverse');
-        }, 150));
-    });
-});
--- src/ps.js
+++ src/ps.js
@@ -3,6 +3,5 @@ require('bootstrap');
 require('./modules/menu');
 require('./modules/chamadas');
 require('./modules/fancybox-config');
-require('./modules/flip');
 require('./modules/lazysizes-config');
 require('./modules/parallax');
