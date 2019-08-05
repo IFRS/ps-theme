c9300886a5864b3a5ac8052787581280a341ba60
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-05 13:56:35 -0300

    Ajuste no carregamento do lazyload para imagens

--- src/modules/lazysizes-config.js
+++ src/modules/lazysizes-config.js
@@ -1,6 +1,7 @@
 require('lazysizes');
 
 $(function() {
+    $('img[data-src]').addClass('lazyload');
+    $('img[data-srcset]').addClass('lazyload');
     $('img[srcset]').attr('data-sizes', 'auto');
-    $('img').addClass('lazyload');
 });
