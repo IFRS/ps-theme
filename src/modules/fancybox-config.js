d91fdb4230b86bf669476929eeb23f2e29aa20dc
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-06 17:11:40 -0300

    Adição de imagens SVG ao fancybox

--- src/modules/fancybox-config.js
+++ src/modules/fancybox-config.js
@@ -1,7 +1,7 @@
 require('@fancyapps/fancybox');
 
 $(function() {
-    $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif']").attr('data-fancybox', 'gallery').fancybox();
+    $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.png'],a[href$='.gif'],a[href$='.svg']").attr('data-fancybox', 'gallery').fancybox();
     $("a[data-fancybox='gallery']").each(function() {
         var caption = $(this).parent().next('.gallery-caption').text();
         if (caption) {
