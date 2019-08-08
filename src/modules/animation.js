4f13dec80f3e68ff35a759ce03f31f29c1993c59
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-08 17:09:56 -0300

    Adição de mais animações, principalmente no cabeçalho

--- src/modules/animation.js
+++ src/modules/animation.js
@@ -1,15 +1,45 @@
+const intersectionObserver = effect => new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > 0) {
+            $(entry.target).addClass(effect);
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
 $(function() {
-    const intersectionObserver = effect => new IntersectionObserver((entries, observer) => {
-        entries.forEach((entry) => {
-            if (entry.intersectionRatio > 0) {
-                $(entry.target).addClass(effect);
-                observer.unobserve(entry.target);
-            }
-        });
+    $('.header__marca').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('slideInDown');
+        observer.observe(e);
+    });
+
+    $('.menu-social__item').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('zoomIn slow');
+        observer.observe(e);
+    });
+
+    $('.menu-principal').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('slideInUp faster');
+        observer.observe(e);
+    });
+
+    $('.menu-principal > .menu-item').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('fadeInRight faster delay-1s');
+        observer.observe(e);
+    });
+
+    $('.widget-atalhos').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('flipInX');
+        observer.observe(e);
     });
 
     $('.btn-formaingresso').addClass('animated').each(function(i, e) {
         let observer = intersectionObserver('fadeInLeft');
+        observer.observe(e);
+    });
+
+    $('.title-sobreposto').addClass('animated').each(function(i, e) {
+        let observer = intersectionObserver('slideInDown');
         observer.observe(e);
     });
 
@@ -20,11 +50,6 @@ $(function() {
 
     $('.home-cursos svg').addClass('animated').each(function(i, e) {
         let observer = intersectionObserver('zoomIn delay-1s');
-        observer.observe(e);
-    });
-
-    $('.title-sobreposto').addClass('animated').each(function(i, e) {
-        let observer = intersectionObserver('slideInDown');
         observer.observe(e);
     });
 
