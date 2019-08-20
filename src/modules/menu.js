69e061a56802164c91f7a02c38fedb5019ceeaa5
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-20 15:33:00 -0300

    Simplificação no funcionamento do menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,21 +3,6 @@ $(function() {
     $('.menu-principal .menu-item-has-children > a').attr('data-toggle', 'dropdown').attr('aria-haspopup', 'true').attr('aria-expanded', 'false');
     $('.menu-principal .sub-menu').addClass('dropdown-menu');
 
-    // Impede que o dropdown se feche ao clicar em um item do mesmo.
-    $('.menu-principal .dropdown-menu').on('click', function(e) {
-        e.stopPropagation();
-    });
-
-    // Impede que o dropdown se feche ao clicar em qualquer lugar da página.
-    $('.menu-principal .menu-item-has-children').on('hide.bs.dropdown', function (e) {
-        if (e.clickEvent && e.clickEvent.target !== this) {
-            return false;
-        }
-    });
-
-    // Abre todos os dropdowns até o item atual.
-    $('.current-menu-parent > a[data-toggle="dropdown"]').dropdown('show');
-
     // Controla a exibição do menu em viewports pequenos.
     function menu_resize_control() {
         if ($(window).width() < 992) {
