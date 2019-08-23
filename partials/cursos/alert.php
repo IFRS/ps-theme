8694c685b4f9d9bc2de9e10ab3d00e6922a5afcd
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2019-08-23 11:29:09 -0300

    Ajuste no texto para mais clareza

--- partials/cursos/alert.php
+++ partials/cursos/alert.php
@@ -2,7 +2,7 @@
     <div class="col-12">
         <div class="alert alert-warning" role="alert">
             <p><strong>&sup1;</strong>&nbsp;Para saber mais sobre a forma de distribui&ccedil;&atilde;o das vagas, leia os <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>">editais</a>.</p>
-            <p><strong>&sup2;</strong>&nbsp;O Curso possui carga hor&aacute;ria a dist&acirc;ncia.</p>
+            <p><strong>&sup2;</strong>&nbsp;O Curso possui parte de sua carga hor&aacute;ria a dist&acirc;ncia.</p>
         </div>
     </div>
 </div>
