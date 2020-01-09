b9d4d178f2c870aa7f815d5a7088b9c56e6b5b1a
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-01-09 13:51:49 -0300

    Remoção da marcação de badge para modalidades nas Chamadas

--- inc/widgets/chamadas.php
+++ inc/widgets/chamadas.php
@@ -4,154 +4,154 @@
  */
 class Chamadas_Widget extends WP_Widget {
 
-	/**
-	 * Register widget with WordPress.
-	 */
-	function __construct() {
-		parent::__construct(
-			'chamadas_widget', // Base ID
-			__( 'Chamadas', 'ifrs-ps-theme' ), // Name
-			array( 'description' => __( 'Chamadas do Processo Seletivo', 'ifrs-ps-theme' ), ) // Args
-		);
-	}
+    /**
+     * Register widget with WordPress.
+     */
+    function __construct() {
+        parent::__construct(
+            'chamadas_widget', // Base ID
+            __( 'Chamadas', 'ifrs-ps-theme' ), // Name
+            array( 'description' => __( 'Chamadas do Processo Seletivo', 'ifrs-ps-theme' ), ) // Args
+        );
+    }
 
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance ) {
-		$formasingresso_all = get_terms(array(
-			'taxonomy' => 'formaingresso',
-			'orderby' => 'name',
-			'fields' => 'ids',
-		));
-		$campi_all = get_terms(array(
-			'taxonomy' => 'campus',
-			'orderby' => 'name',
-			'fields' => 'ids',
-		));
+    /**
+     * Front-end display of widget.
+     *
+     * @see WP_Widget::widget()
+     *
+     * @param array $args     Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget( $args, $instance ) {
+        $formasingresso_all = get_terms(array(
+            'taxonomy' => 'formaingresso',
+            'orderby' => 'name',
+            'fields' => 'ids',
+        ));
+        $campi_all = get_terms(array(
+            'taxonomy' => 'campus',
+            'orderby' => 'name',
+            'fields' => 'ids',
+        ));
 
-		$chamadas = array();
+        $chamadas = array();
 
-		foreach ($formasingresso_all as $id1) {
-			foreach ($campi_all as $id2) {
-				$chamadas_query = new WP_Query(array(
-					'post_type' => 'chamada',
-					'post_status' => 'publish',
-					'posts_per_page' => '-1',
-					'tax_query' => array(
-						'relation' => 'AND',
-						array(
-							'taxonomy' => 'formaingresso',
-							'field'    => 'term_id',
-							'terms'    => $id1,
-						),
-						array(
-							'taxonomy' => 'campus',
-							'field'    => 'term_id',
-							'terms'    => $id2,
-						),
-					)
-				));
-				if ($chamadas_query->have_posts()) {
-					while ($chamadas_query->have_posts()) {
-						$chamadas_query->the_post();
-						$chamadas[$id1][$id2][] = get_post();
-					}
-				}
-			}
-		}
+        foreach ($formasingresso_all as $id1) {
+            foreach ($campi_all as $id2) {
+                $chamadas_query = new WP_Query(array(
+                    'post_type' => 'chamada',
+                    'post_status' => 'publish',
+                    'posts_per_page' => '-1',
+                    'tax_query' => array(
+                        'relation' => 'AND',
+                        array(
+                            'taxonomy' => 'formaingresso',
+                            'field'    => 'term_id',
+                            'terms'    => $id1,
+                        ),
+                        array(
+                            'taxonomy' => 'campus',
+                            'field'    => 'term_id',
+                            'terms'    => $id2,
+                        ),
+                    )
+                ));
+                if ($chamadas_query->have_posts()) {
+                    while ($chamadas_query->have_posts()) {
+                        $chamadas_query->the_post();
+                        $chamadas[$id1][$id2][] = get_post();
+                    }
+                }
+            }
+        }
 
-		echo $args['before_widget'];
-		if ( ! empty( $instance['title'] ) ) {
-			echo $args['before_title'];
-		    echo apply_filters('widget_title', $instance['title']);
-			echo $args['after_title'];
-		}
-		?>
-		<div class="widget-chamadas__content">
-			<div class="chamadas" aria-live="polite">
-				<div id="formasingresso" class="chamadas__formasingresso">
-					<p class="chamadas__text"><?php _e('Selecione a sua forma de ingresso.', 'ifrs-ps-theme'); ?><br><small><?php _e('Os resultados de cada forma de ingresso serão divulgados conforme cronograma.', 'ifrs-ps=theme'); ?></small></p>
-					<?php foreach ($chamadas as $formaingresso_id => $campi) : ?>
-						<?php $formaingresso_obj = get_term($formaingresso_id); ?>
-						<a class="btn btn-formaingresso btn-lg btn-block toggle" href="#campi-<?php echo $formaingresso_obj->slug; ?>"><span class="sr-only">Ingresso por </span><?php echo $formaingresso_obj->name; ?></a>
-						<div id="campi-<?php echo $formaingresso_obj->slug; ?>" class="chamadas__campi">
-							<p class="chamadas__text">Selecione o seu Campus.</p>
-							<ol class="breadcrumb">
-								<li class="breadcrumb-item"><a href="#formasingresso" class="breadcrumb-formaingresso">Formas de Ingresso</a></li>
-								<li class="breadcrumb-item active" aria-current="page"><?php echo $formaingresso_obj->name; ?></li>
-							</ol>
-							<?php foreach ($campi as $campus_id => $chamada) : ?>
-								<?php $campus_obj = get_term($campus_id); ?>
-								<a class="btn btn-campus btn-block toggle" href="#chamadas-<?php echo $campus_obj->slug; ?>-<?php echo $formaingresso_obj->slug; ?>"><span class="sr-only">Campus </span><?php echo $campus_obj->name; ?></a>
-								<div id="chamadas-<?php echo $campus_obj->slug; ?>-<?php echo $formaingresso_obj->slug; ?>" class="chamadas__list">
-									<p class="chamadas__text">Confira abaixo as chamadas j&aacute; realizadas.</p>
-									<ol class="breadcrumb">
-										<li class="breadcrumb-item"><a href="#formasingresso" class="breadcrumb-formaingresso">Formas de Ingresso</a></li>
-										<li class="breadcrumb-item"><a href="#campi-<?php echo $formaingresso_obj->slug; ?>" class="breadcrumb-campus"><?php echo $formaingresso_obj->name; ?></a></li>
-										<li class="breadcrumb-item active" aria-current="page"><?php echo $campus_obj->name; ?></li>
-									</ol>
-									<?php foreach ($chamada as $resultado) : ?>
-										<div class="chamada">
-											<h3 class="chamada__title"><a href="<?php echo get_permalink($resultado); ?>" rel="bookmark"><?php echo $resultado->post_title; ?></a></h3>
-											<p class="chamada__meta"><?php echo get_the_time('d/m/Y', $resultado); ?></p>
-											<div class="chamada__badges">
-												<?php $modalidades = get_post_meta($resultado->ID, '_chamada_resultados_group'); ?>
-												<?php foreach ($modalidades[0] as $id => $modalidade) : ?>
-													<span class="badge badge-modalidade"><?php echo get_term($modalidade['modalidade'], 'modalidade')->name; ?></span>
-												<?php endforeach; ?>
-											</div>
-										</div>
-									<?php endforeach; ?>
-								</div>
-							<?php endforeach; ?>
-						</div>
-					<?php endforeach ?>
-				</div>
-			</div>
-		</div>
+        echo $args['before_widget'];
+        if ( ! empty( $instance['title'] ) ) {
+            echo $args['before_title'];
+            echo apply_filters('widget_title', $instance['title']);
+            echo $args['after_title'];
+        }
+        ?>
+        <div class="widget-chamadas__content">
+            <div class="chamadas" aria-live="polite">
+                <div id="formasingresso" class="chamadas__formasingresso">
+                    <p class="chamadas__text"><?php _e('Selecione a sua forma de ingresso.', 'ifrs-ps-theme'); ?><br><small><?php _e('Os resultados de cada forma de ingresso serão divulgados conforme cronograma.', 'ifrs-ps=theme'); ?></small></p>
+                    <?php foreach ($chamadas as $formaingresso_id => $campi) : ?>
+                        <?php $formaingresso_obj = get_term($formaingresso_id); ?>
+                        <a class="btn btn-formaingresso btn-lg btn-block toggle" href="#campi-<?php echo $formaingresso_obj->slug; ?>"><span class="sr-only">Ingresso por </span><?php echo $formaingresso_obj->name; ?></a>
+                        <div id="campi-<?php echo $formaingresso_obj->slug; ?>" class="chamadas__campi">
+                            <p class="chamadas__text">Selecione o seu Campus.</p>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#formasingresso" class="breadcrumb-formaingresso">Formas de Ingresso</a></li>
+                                <li class="breadcrumb-item active" aria-current="page"><?php echo $formaingresso_obj->name; ?></li>
+                            </ol>
+                            <?php foreach ($campi as $campus_id => $chamada) : ?>
+                                <?php $campus_obj = get_term($campus_id); ?>
+                                <a class="btn btn-campus btn-block toggle" href="#chamadas-<?php echo $campus_obj->slug; ?>-<?php echo $formaingresso_obj->slug; ?>"><span class="sr-only">Campus </span><?php echo $campus_obj->name; ?></a>
+                                <div id="chamadas-<?php echo $campus_obj->slug; ?>-<?php echo $formaingresso_obj->slug; ?>" class="chamadas__list">
+                                    <p class="chamadas__text">Confira abaixo as chamadas j&aacute; realizadas.</p>
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#formasingresso" class="breadcrumb-formaingresso">Formas de Ingresso</a></li>
+                                        <li class="breadcrumb-item"><a href="#campi-<?php echo $formaingresso_obj->slug; ?>" class="breadcrumb-campus"><?php echo $formaingresso_obj->name; ?></a></li>
+                                        <li class="breadcrumb-item active" aria-current="page"><?php echo $campus_obj->name; ?></li>
+                                    </ol>
+                                    <?php foreach ($chamada as $resultado) : ?>
+                                        <div class="chamada">
+                                            <h3 class="chamada__title"><a href="<?php echo get_permalink($resultado); ?>" rel="bookmark"><?php echo $resultado->post_title; ?></a></h3>
+                                            <p class="chamada__meta"><?php echo get_the_time('d/m/Y', $resultado); ?></p>
+                                            <div class="chamada__badges">
+                                                <?php $modalidades = get_post_meta($resultado->ID, '_chamada_resultados_group'); ?>
+                                                <?php foreach ($modalidades[0] as $id => $modalidade) : ?>
+                                                    <?php echo get_term($modalidade['modalidade'], 'modalidade')->name; ?><?php echo ($id !== array_key_last($modalidades[0])) ? ', ' : ''; ?>
+                                                <?php endforeach; ?>
+                                            </div>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+            </div>
+        </div>
         <?php
-		echo $args['after_widget'];
-	}
+        echo $args['after_widget'];
+    }
 
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Chamadas', 'ifrs-ps-theme' );
-		?>
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
-		<?php
-	}
+    /**
+     * Back-end widget form.
+     *
+     * @see WP_Widget::form()
+     *
+     * @param array $instance Previously saved values from database.
+     */
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Chamadas', 'ifrs-ps-theme' );
+        ?>
+        <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+        </p>
+        <?php
+    }
 
-	/**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+    /**
+     * Sanitize widget form values as they are saved.
+     *
+     * @see WP_Widget::update()
+     *
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     *
+     * @return array Updated safe values to be saved.
+     */
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 
-		return $instance;
-	}
+        return $instance;
+    }
 
 } // class Foo_Widget
 
