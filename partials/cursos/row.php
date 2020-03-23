987e67515c0574a64f23f7ce0909d6372d0fed3f
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2020-03-23 14:57:11 -0300

    Ajuste na marcação da tabela de Cursos

--- partials/cursos/row.php
+++ partials/cursos/row.php
@@ -2,20 +2,21 @@
     <td><a href="<?php echo get_permalink() ?>"><?php the_title(); ?></a><?php echo get_post_meta( get_the_ID(), '_curso_ead', 1 ) ? '&sup2;' : '' ?></td>
     <td>
         <?php foreach (get_the_terms(get_the_ID(), 'campus') as $campus) : ?>
-            <p><?php echo $campus->name; ?></p>
+            <?php echo $campus->name; ?>
         <?php endforeach; ?>
     </td>
     <td>
         <?php foreach (get_the_terms(get_the_ID(), 'modalidade') as $modalidade) : ?>
-            <p><?php echo $modalidade->name; ?></p>
+            <?php echo $modalidade->name; ?>
         <?php endforeach; ?>
     </td>
     <td>
-        <?php foreach (get_the_terms(get_the_ID(), 'turno') as $turno) : ?>
-            <p><?php echo $turno->name; ?></p>
+        <?php $turnos = get_the_terms(get_the_ID(), 'turno'); ?>
+        <?php foreach ($turnos as $key => $turno) : ?>
+            <?php echo $turno->name; echo ($key !== array_key_last($turnos)) ? ',' : ''; ?>
         <?php endforeach; ?>
     </td>
     <td class="text-center">
-        <p><?php echo get_post_meta(get_the_ID(), '_curso_vagas', true); ?></p>
+        <?php echo get_post_meta(get_the_ID(), '_curso_vagas', true); ?>
     </td>
 </tr>
