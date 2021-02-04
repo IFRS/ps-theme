2ded14f75e0f1103f46e77de17b0408a1668697e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-02-04 13:39:13 -0300

    Ajustes nos assets e adição de modificação nas tags script

--- inc/assets.php
+++ inc/assets.php
@@ -7,34 +7,46 @@ add_action( 'wp_enqueue_scripts', function() {
         wp_deregister_style( 'wp-block-library' );
     }
 
-    wp_enqueue_style('css-vendor', get_stylesheet_directory_uri().'/css/vendor.css', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/vendor.css'), 'all');
+    wp_enqueue_style('vendor', get_stylesheet_directory_uri().'/css/vendor.css', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/vendor.css'), 'all');
 
-    wp_enqueue_style('css-ps', get_stylesheet_directory_uri().'/css/ps.css', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/ps.css'), 'all');
+    wp_enqueue_style('ps', get_stylesheet_directory_uri().'/css/ps.css', array('vendor'), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/css/ps.css'), 'all');
 }, 1 );
 
 add_action( 'wp_enqueue_scripts', function() {
     /* wp_register_script( $handle, $src, $deps, $ver, $in_footer ); */
     /* wp_enqueue_script( $handle[, $src, $deps, $ver, $in_footer] ); */
 
-    wp_enqueue_script( 'js-ie', get_template_directory_uri().'/js/ie.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/ie.js'), false );
-    wp_script_add_data( 'js-ie', 'conditional', 'lt IE 9' );
+    wp_enqueue_script( 'ie', get_template_directory_uri().'/js/ie.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/ie.js'), false );
+    wp_script_add_data( 'ie', 'conditional', 'lt IE 9' );
 
-    wp_enqueue_script('js-commons', get_template_directory_uri().'/js/commons.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/commons.js'), true);
+    wp_enqueue_script('commons', get_template_directory_uri().'/js/commons.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/commons.js'), true);
 
-    wp_enqueue_script('js-ps', get_template_directory_uri().'/js/ps.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/ps.js'), true);
+    wp_enqueue_script('ps', get_template_directory_uri().'/js/ps.js', array('commons'), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/ps.js'), true);
 
     if (is_post_type_archive( 'curso' ) || is_tax('modalidade')) {
-        wp_enqueue_script('js-cursos', get_template_directory_uri().'/js/cursos.js', array(), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/cursos.js'), true);
+        wp_enqueue_script('cursos', get_template_directory_uri().'/js/cursos.js', array('commons'), WP_DEBUG ? null : filemtime(get_stylesheet_directory() . '/js/cursos.js'), true);
     }
 
     if (!WP_DEBUG) {
-        wp_enqueue_script( 'js-barra-brasil', 'https://barra.brasil.gov.br/barra.js', array(), null, true );
+        wp_enqueue_script( 'barra-brasil', 'https://barra.brasil.gov.br/barra.js', array(), null, true );
     }
-}, 2 );
+}, 1 );
 
-add_action('wp_head', function() {
-    if (!WP_DEBUG) {
-        echo '<link rel="preconnect" href="https://barra.brasil.gov.br">';
-        echo '<link rel="preconnect" href="https://vlibras.gov.br">';
+add_filter('script_loader_tag', function($tag, $handle) {
+    $scripts_to_defer = array('barra-brasil');
+    $scripts_to_async = array();
+
+    foreach ($scripts_to_defer as $defer_script) {
+        if ($defer_script === $handle) {
+            return str_replace(' src', ' defer="defer" src', $tag);
+        }
     }
-}, 0);
+
+    foreach ($scripts_to_async as $async_script) {
+        if ($async_script === $handle) {
+            return str_replace(' src', ' async="async" src', $tag);
+        }
+    }
+
+    return $tag;
+}, 2, 2);
