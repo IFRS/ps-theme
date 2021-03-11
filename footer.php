6f5a0c80caa68523c60c92a4b92306150dd22cea
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-11 17:46:18 -0300

    Remoção do endereço físico

--- footer.php
+++ footer.php
@@ -34,10 +34,7 @@
             </div>
             <div class="col-12 col-md-7 col-lg-8 col-xl-9">
                 <address class="contato">
-                    <p>Rua General Os&oacute;rio, 348 | Bairro Centro</p>
-                    <p>CEP: 95700-086 | Bento Gon&ccedil;alves/RS</p>
                     <p>E-mail: <a href="mailto:processoseletivo@ifrs.edu.br">processoseletivo@ifrs.edu.br</a></p>
-                    <p>Telefone: (54) 3449-3300</p>
                 </address>
             </div>
         </div>
