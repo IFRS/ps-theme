93b6a391a32b982b56fe762ab10d38955db6f29e
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-15 16:47:27 -0300

    Adição de tooltips automáticos nas redes sociais

--- src/modules/tooltips.js
+++ src/modules/tooltips.js
@@ -1,3 +1,11 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+$('.menu-social__link').each(function (i, e) {
+    texto = $(e).children('span').first().html();
+    $(e).tooltip({
+        placement: 'top',
+        title: texto.trim(),
+    });
+});
