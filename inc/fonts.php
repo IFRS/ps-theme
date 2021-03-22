2318c2b5d889c549c29bf307a5c8001f76c466ef
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-22 13:34:32 -0300

    Adição de nova fonte para títulos

--- inc/fonts.php
+++ inc/fonts.php
@@ -9,5 +9,16 @@ add_action('wp_head', function() {
     <noscript>
         <link rel="stylesheet" href="<?php echo $fonts_file; ?>"/>
     </noscript>
+
+    <?php $google_fonts = 'https://fonts.googleapis.com/css2?family=Rubik:wght@800&display=swap'; ?>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+    <link rel="preload" as="style" href="<?php echo $google_fonts; ?>" />
+
+    <link rel="stylesheet" href="<?php echo $google_fonts; ?>" media="print" onload="this.media='all'" />
+
+    <noscript>
+        <link rel="stylesheet" href="<?php echo $google_fonts; ?>" />
+    </noscript>
 <?php
 }, 1);
