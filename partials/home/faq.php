3244d60fbaba5705d55e4be938d484c3b1d27186
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-03-23 14:14:39 -0300

    Ajustes nas perguntas para compatibilidade com list.js

--- partials/home/faq.php
+++ partials/home/faq.php
@@ -1,5 +1,8 @@
-<article class="home-faq">
+<article class="home-faq" id="home-faq">
     <h2 class="home-faq__title">Voc&ecirc; tem alguma d&uacute;vida?</h2>
+    <?php $field_id = uniqid(); ?>
+    <label for="<?php echo $field_id; ?>" class="sr-only">Buscar nas perguntas</label>
+    <input type="search" id="<?php echo $field_id; ?>" class="form-control form-control-sm home-faq__busca search" placeholder="buscar nas perguntas...">
     <?php
         $args = array(
             'post_type' => 'pergunta',
@@ -12,12 +15,12 @@
         $perguntas = new WP_Query($args);
     ?>
     <?php if ($perguntas->have_posts()) : ?>
-        <div class="home-faq__perguntas" id="home-faq__perguntas">
+        <ul class="home-faq__perguntas" id="home-faq__perguntas" aria-live="polite">
         <?php while ($perguntas->have_posts()) : $perguntas->the_post(); ?>
-            <div class="card home-faq__item">
+            <li class="card home-faq__item">
                 <div class="card-header" id="heading<?php the_ID(); ?>">
                     <h4 class="mb-0 home-faq__item-title">
-                        <a href="<?php the_permalink(); ?>" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapse<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse<?php the_ID(); ?>">
+                        <a href="<?php the_permalink(); ?>" class="btn btn-link collapsed home-faq__item-link" data-toggle="collapse" data-target="#collapse<?php the_ID(); ?>" aria-expanded="true" aria-controls="collapse<?php the_ID(); ?>">
                             <?php the_title(); ?>
                         </a>
                     </h4>
@@ -28,9 +31,9 @@
                         <?php the_content(); ?>
                     </div>
                 </div>
-            </div>
+            </li>
         <?php endwhile; ?>
-        </div>
+        </ul>
     <?php else : ?>
         <div class="alert alert-warning" role="alert">
             <p><strong>Ops!</strong> Ainda n&atilde;o existem perguntas cadastradas.</p>
