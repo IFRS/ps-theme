567eae12ee5191ea78e8f8ea63f38e6a8028db23
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-07-29 16:37:04 -0300

    Preload da imagem de fundo do cabeçalho

--- inc/resource-hints.php
+++ inc/resource-hints.php
@@ -2,6 +2,7 @@
 if (!WP_DEBUG) {
     add_action('wp_head', function() {
 ?>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/img/header-bg.png" as="image" crossorigin="anonymous"/>
     <link rel="preconnect" href="https://vlibras.gov.br">
 <?php
     }, 0);
