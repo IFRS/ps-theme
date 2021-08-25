4d916ba597f20da9ae98680dae259c596e3aa3e6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-25 11:25:38 -0300

    Ajuste no pré-carregamento de fontes

--- inc/fonts.php
+++ inc/fonts.php
@@ -2,8 +2,8 @@
 add_action('wp_head', function() {
     $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/Regular/OpenSans-Regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/Bold/OpenSans-Bold.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/opensans/open-sans-v23-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/opensans/open-sans-v23-latin-700.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
     <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
