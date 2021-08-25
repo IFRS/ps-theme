33cc85ddc914057c946145dae0996573ad8564ad
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-25 12:52:28 -0300

    Uso da fonte Inter em detrimento da OpenSans

--- inc/fonts.php
+++ inc/fonts.php
@@ -2,24 +2,11 @@
 add_action('wp_head', function() {
     $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/opensans/open-sans-v23-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/opensans/open-sans-v23-latin-700.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/inter-v3-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
     <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
         <link rel="stylesheet" href="<?php echo $fonts_file; ?>"/>
     </noscript>
-
-    <?php $google_fonts = 'https://fonts.googleapis.com/css2?family=Rubik:wght@800&display=swap'; ?>
-    <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-
-    <link rel="preload" as="style" href="<?php echo $google_fonts; ?>" />
-
-    <link rel="stylesheet" href="<?php echo $google_fonts; ?>" media="print" onload="this.media='all'" />
-
-    <noscript>
-        <link rel="stylesheet" href="<?php echo $google_fonts; ?>" />
-    </noscript>
 <?php
-}, 1);
+}, 0);
