6eb0225b01d6646ac6c21caba2c69204367e637b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-27 10:12:41 -0300

    Melhoria na marcação de Editais

--- archive-edital.php
+++ archive-edital.php
@@ -1,18 +1,8 @@
 <?php get_header(); ?>
 
-<section class="container">
-    <div class="editais">
-        <div class="row">
-            <div class="col-12">
-                <h2 class="editais__title">Editais</h2>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <?php echo get_template_part('partials/editais/loop'); ?>
-            </div>
-        </div>
-    </div>
+<section class="container editais">
+    <h2 class="editais__title">Editais</h2>
+    <?php echo get_template_part('partials/editais/loop'); ?>
 </section>
 
 <?php get_footer(); ?>
