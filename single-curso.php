be89a185546bf41a4be18da5acf34d62cd74571c
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-27 12:02:22 -0300

    Mlehorias na marcação para Curso

--- single-curso.php
+++ single-curso.php
@@ -6,98 +6,86 @@
     <div class="row">
         <div class="col">
             <article class="post">
-                <div class="row">
-                    <div class="col-12">
-                        <h2 class="post__title"><?php the_title(); ?></h2>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="post__content">
-                            <?php the_content(); ?>
-                            <hr>
-                            <div class="row">
-                                <div class="col-12 col-md-6">
-                                    <p>
-                                        <strong>Dura&ccedil;&atilde;o: </strong>
-                                        <?php
-                                            $duracao = get_post_meta(get_the_ID(), '_curso_duracao', true);
-                                            if (!empty($duracao)) {
-                                                echo $duracao;
-                                            } else {
-                                                echo '-';
-                                            }
-                                        ?>
-                                    </p>
+                <h2 class="post__title"><?php the_title(); ?></h2>
+                <div class="post__content">
+                    <?php the_content(); ?>
+                    <hr>
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <p>
+                                <strong>Dura&ccedil;&atilde;o: </strong>
+                                <?php
+                                    $duracao = get_post_meta(get_the_ID(), '_curso_duracao', true);
+                                    if (!empty($duracao)) {
+                                        echo $duracao;
+                                    } else {
+                                        echo '-';
+                                    }
+                                ?>
+                            </p>
 
-                                    <p>
-                                        <strong>Vagas: </strong>
-                                        <?php
-                                            $vagas = get_post_meta(get_the_ID(), '_curso_vagas', true);
-                                            if (!empty($vagas)) {
-                                                echo $vagas;
-                                            } else {
-                                                echo '-';
-                                            }
-                                        ?>
-                                    </p>
+                            <p>
+                                <strong>Vagas: </strong>
+                                <?php
+                                    $vagas = get_post_meta(get_the_ID(), '_curso_vagas', true);
+                                    if (!empty($vagas)) {
+                                        echo $vagas;
+                                    } else {
+                                        echo '-';
+                                    }
+                                ?>
+                            </p>
 
-                                    <?php $turnos = get_the_terms(get_the_ID(), 'turno'); ?>
-                                    <?php $turnos_counter = 1; ?>
-                                    <p>
-                                        <strong>Turnos: </strong>
-                                        <?php
-                                            if (!empty($turnos)) {
-                                                foreach ($turnos as $turno) :
-                                                    echo ($turnos_counter == sizeof($turnos) ? $turno->name : $turno->name . ', ');
-                                                    $turnos_counter++;
-                                                endforeach;
-                                            } else {
-                                                echo '-';
-                                            }
-                                        ?>
-                                    </p>
-                                </div>
-                                <div class="col-12 col-md-6">
-                                    <?php $modalidades = get_the_terms(get_the_ID(), 'modalidade'); ?>
-                                    <p>
-                                        <strong>Modalidade de ensino: </strong>
-                                        <?php
-                                            if (!empty($modalidades)) {
-                                                foreach ($modalidades as $modalidade) {
-                                                    echo $modalidade->name;
-                                                }
-                                            } else {
-                                                echo '-';
-                                            }
-                                        ?>
-                                    </p>
+                            <?php $turnos = get_the_terms(get_the_ID(), 'turno'); ?>
+                            <?php $turnos_counter = 1; ?>
+                            <p>
+                                <strong>Turnos: </strong>
+                                <?php
+                                    if (!empty($turnos)) {
+                                        foreach ($turnos as $turno) :
+                                            echo ($turnos_counter == sizeof($turnos) ? $turno->name : $turno->name . ', ');
+                                            $turnos_counter++;
+                                        endforeach;
+                                    } else {
+                                        echo '-';
+                                    }
+                                ?>
+                            </p>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <?php $modalidades = get_the_terms(get_the_ID(), 'modalidade'); ?>
+                            <p>
+                                <strong>Modalidade de ensino: </strong>
+                                <?php
+                                    if (!empty($modalidades)) {
+                                        foreach ($modalidades as $modalidade) {
+                                            echo $modalidade->name;
+                                        }
+                                    } else {
+                                        echo '-';
+                                    }
+                                ?>
+                            </p>
 
-                                    <?php $campi = get_the_terms(get_the_ID(), 'campus'); ?>
-                                    <p>
-                                        <strong>Curso oferecido no Campus: </strong>
-                                        <?php
-                                            if (!empty($campi)) {
-                                                foreach ($campi as $campus) {
-                                                    echo $campus->name;
-                                                }
-                                            } else {
-                                                echo '-';
-                                            }
-                                        ?>
-                                    </p>
-                                </div>
-                            </div>
-                            <?php if (get_post_meta( get_the_ID(), '_curso_ead', 1 )) : ?>
-                                <hr>
-                                <div class="row">
-                                    <div class="col-12">
-                                        <div class="alert alert-info" role="alert"><p><small><strong>Fique atento!</strong>&nbsp;Esse Curso possui parte da carga hor&aacute;ria a dist&acirc;ncia.</small></p></div>
-                                    </div>
-                                </div>
-                            <?php endif; ?>
+                            <?php $campi = get_the_terms(get_the_ID(), 'campus'); ?>
+                            <p>
+                                <strong>Curso oferecido no Campus: </strong>
+                                <?php
+                                    if (!empty($campi)) {
+                                        foreach ($campi as $campus) {
+                                            echo $campus->name;
+                                        }
+                                    } else {
+                                        echo '-';
+                                    }
+                                ?>
+                            </p>
                         </div>
                     </div>
+                    <?php if (get_post_meta( get_the_ID(), '_curso_ead', 1 )) : ?>
+                        <hr>
+                        <div class="alert alert-info" role="alert"><p><small><strong>Fique atento!</strong>&nbsp;Esse Curso possui parte da carga hor&aacute;ria a dist&acirc;ncia.</small></p></div>
+                    <?php endif; ?>
                 </div>
             </article>
         </div>
