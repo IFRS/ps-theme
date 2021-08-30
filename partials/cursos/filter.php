8308423be1d9cdd4e7c4804dd015b442027ee4e9
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-08-30 11:27:13 -0300

    Ajustes na lista de cursos

--- partials/cursos/filter.php
+++ partials/cursos/filter.php
@@ -60,7 +60,7 @@
 
         <a href="<?php echo get_post_type_archive_link( 'curso' ); ?>" class="btn btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Limpar Filtros">
             <span class="visually-hidden">Limpar Filtros</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#999999" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs">
                 <polyline points="3 6 5 6 21 6"></polyline>
                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
                 <line x1="10" y1="11" x2="10" y2="17"></line>
