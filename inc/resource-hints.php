52ef156e6ef0dab28a0123230d03a1222d395c09
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2021-09-01 10:44:20 -0300

    Remoção de pré-carregamento para imagem de cabeçalho

--- inc/resource-hints.php
+++ inc/resource-hints.php
@@ -1,9 +1,3 @@
-<?php
-if (!WP_DEBUG) {
-    add_action('wp_head', function() {
-?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/img/header-bg.png" as="image" crossorigin="anonymous"/>
+<?php add_action('wp_head', function() { ?>
     <link rel="preconnect" href="https://vlibras.gov.br">
-<?php
-    }, 0);
-}
+<?php }, 0);
