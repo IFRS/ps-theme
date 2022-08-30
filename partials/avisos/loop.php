cc4f2c5e4549321d5cb6859496630db09b536aed
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-08-30 14:44:40 -0300

    Ajustes nos cards dos avisos

--- partials/avisos/loop.php
+++ partials/avisos/loop.php
@@ -3,19 +3,15 @@
     <?php while (have_posts()) : the_post(); ?>
         <div class="col">
             <article class="card">
-                <?php if (!is_category()) : ?>
-                <div class="card-header">
-                    <?php $cats = get_the_category(); ?>
-                    <?php if (!empty($cats)) : ?>
-                        <ul class="list-inline mb-0">
-                            <?php foreach ($cats as $key => $cat) : ?>
-                                <li class="list-inline-item"><a href="<?php echo get_category_link( $cat->term_id ); ?>" class="text-muted"><?php echo $cat->name; ?></a></li>
-                            <?php endforeach; ?>
-                        </ul>
-                    <?php endif; ?>
-                </div>
-                <?php endif; ?>
                 <div class="card-body">
+                    <?php $cats = get_the_category(); ?>
+                        <?php if (!empty($cats)) : ?>
+                            <ul class="list-inline">
+                                <?php foreach ($cats as $key => $cat) : ?>
+                                    <li class="list-inline-item"><a href="<?php echo get_category_link( $cat->term_id ); ?>" class="text-muted"><?php echo $cat->name; ?></a></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
                     <h3 class="card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <small class="d-block text-muted mb-2"><?php the_date(); ?></small>
                     <div class="card-text"><?php the_excerpt(); ?></div>
