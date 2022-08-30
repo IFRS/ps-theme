c22407bffc331b32e4410755b896c90648d88ff6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-08-30 12:00:45 -0300

    Ajuste no espaçamento da lista de avisos

--- partials/avisos/loop.php
+++ partials/avisos/loop.php
@@ -1,5 +1,5 @@
 <?php if (have_posts()) : ?>
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mb-3">
     <?php while (have_posts()) : the_post(); ?>
         <div class="col">
             <article class="card">
