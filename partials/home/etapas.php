61e105bad92ad73eaeb9cc2d061b27d0f1a2e441
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-09-05 14:40:05 -0300

    Melhoria na timeline com as etapas importantes

--- partials/home/etapas.php
+++ partials/home/etapas.php
@@ -27,7 +27,7 @@
     ),
   ));
 
-  // $marco_anterior = get_posts(array(
+  // $marcos_anteriores = get_posts(array(
   //   'post_type'      => 'evento',
   //   'post_status'    => 'publish',
   //   'posts_per_page' => 1,
@@ -50,7 +50,7 @@
   $marcos_futuros = get_posts(array(
     'post_type'      => 'evento',
     'post_status'    => 'publish',
-    'posts_per_page' => 2,
+    'posts_per_page' => count($marcos_atuais) === 0 ? 2 : 1,
     'orderby'        => 'meta_value_num',
     'order'          => 'ASC',
     'meta_key'       => array('_evento_data-inicio', '_evento_data-fim'),
@@ -70,16 +70,23 @@
 
 <?php if (!empty($marcos_atuais) || !empty($marcos_futuros)) : ?>
   <div class="container">
-    <a class="timeline" href="<?php echo get_post_type_archive_link('evento'); ?>">
-      <h2 class="timeline__title">Etapas</h2>
-      <ul class="timeline__list">
-      <?php foreach ($marcos_atuais as $marco) : ?>
-        <li class="timeline__item timeline__item--atual"><?php echo $marco->post_title; ?></li>
-      <?php endforeach; ?>
-      <?php foreach ($marcos_futuros as $marco) : ?>
-        <li class="timeline__item"><?php echo $marco->post_title; ?></li>
-      <?php endforeach; ?>
-      </ul>
+    <a class="timeline" href="<?php echo get_post_type_archive_link('evento'); ?>" data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Ir para o Cronograma Completo">
+      <?php if (!empty($marcos_atuais)) : ?>
+        <h2 class="timeline__title timeline__title--atual"><?php echo _n('Etapa Atual','Etapas Atuais',count($marcos_atuais),'ifrs-ps-theme'); ?></h2>
+        <ul class="timeline__list">
+        <?php foreach ($marcos_atuais as $marco) : ?>
+          <li class="timeline__item timeline__item--atual"><?php echo $marco->post_title; ?></li>
+        <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
+      <?php if (!empty($marcos_futuros)) : ?>
+        <h2 class="timeline__title"><?php echo _n('Pr&oacute;xima Etapa','Pr&oacute;ximas Etapas',count($marcos_atuais),'ifrs-ps-theme'); ?></h2>
+        <ul class="timeline__list">
+        <?php foreach ($marcos_futuros as $marco) : ?>
+          <li class="timeline__item"><?php echo $marco->post_title; ?></li>
+        <?php endforeach; ?>
+        </ul>
+      <?php endif; ?>
     </a>
   </div>
 <?php endif; ?>
