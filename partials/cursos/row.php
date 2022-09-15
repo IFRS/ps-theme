ba07363e5cdaf1d5fae3aec5a8f4bd8fb1f280ac
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-09-15 11:14:58 -0300

    Verificação das formas de ingresso

--- partials/cursos/row.php
+++ partials/cursos/row.php
@@ -13,13 +13,15 @@
     <td>
         <?php $formasingresso = get_the_terms(get_the_ID(), 'formaingresso'); ?>
         <?php
-            foreach ($formasingresso as $key => $formaingresso) {
-                if (!empty($formaingresso->description)) {
-                    printf('<span class="formaingresso-help" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="%s">%s</span>', $formaingresso->description, $formaingresso->name);
-                } else {
-                    echo $formaingresso->name;
+            if ($formasingresso) {
+                foreach ($formasingresso as $key => $formaingresso) {
+                    if (!empty($formaingresso->description)) {
+                        printf('<span class="formaingresso-help" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="%s">%s</span>', $formaingresso->description, $formaingresso->name);
+                    } else {
+                        echo $formaingresso->name;
+                    }
+                    echo ($key !== array_key_last($formasingresso)) ? ', ' : '';
                 }
-                echo ($key !== array_key_last($formasingresso)) ? ', ' : '';
             }
         ?>
     </td>
