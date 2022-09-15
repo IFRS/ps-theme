355ffee5e15dbc8f3adcb31c5b3009e7216d3180
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2022-09-15 14:33:17 -0300

    Adição de delay na busca por perguntas

--- src/modules/faq.js
+++ src/modules/faq.js
@@ -1,8 +1,9 @@
 import List from 'list.js';
 
 let faqList = new List('faq', {
+  searchDelay: 500,
   listClass: 'faq__perguntas',
-  valueNames: ['accordion-header']
+  valueNames: ['accordion-header'],
 });
 
 export { faqList }
