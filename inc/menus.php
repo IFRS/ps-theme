4b48eb2233129006a445d07aaf153af7270b0d19
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-04-03 16:51:29 -0300

    Mudança no local de aplicação do filtro para adição de itens no menu

--- inc/menus.php
+++ inc/menus.php
@@ -41,3 +41,26 @@ add_filter( 'nav_menu_link_attributes', function( $atts, $item, $args, $depth ) 
 
     return $atts;
 }, 10, 4 );
+
+/* Adiciona items automaticamente ao menu, conforme configurações */
+add_filter( 'wp_nav_menu_items', function( $items ) {
+    $now = time() - (3 * 60 * 60); // Hora atual em UTC-3
+
+    $inscricao_url = cmb2_get_option( 'programacao_options', 'inscricao_url' );
+    $inscricao_inicio = cmb2_get_option( 'programacao_options', 'inscricao_inicio' );
+    $inscricao_fim = cmb2_get_option( 'programacao_options', 'inscricao_fim' );
+
+    $matricula_url = cmb2_get_option( 'programacao_options', 'matricula_url' );
+    $matricula_inicio = cmb2_get_option( 'programacao_options', 'matricula_inicio' );
+    $matricula_fim = cmb2_get_option( 'programacao_options', 'matrio$matricula_fim' );
+
+    if ($inscricao_url && $now >= $inscricao_inicio && $now <= $inscricao_fim) {
+        $items .= '<li class="nav-item"><a class="nav-link" href="' . $inscricao_url . '">Inscri&ccedil;&otilde;es</a></li>';
+    }
+
+    if ($matricula_url && $now >= $matricula_inicio && $now <= $matricula_fim) {
+        $items .= '<li class="nav-item"><a class="nav-link" href="' . $matricula_url . '">Inscri&ccedil;&otilde;es</a></li>';
+    }
+
+    return $items;
+} );
--- partials/menu.php
+++ partials/menu.php
@@ -1,27 +1,3 @@
-<?php
-    add_filter( 'wp_nav_menu_items', function( $items ) {
-        $now = time() - (3 * 60 * 60);
-
-        $inscricao_url = cmb2_get_option( 'programacao_options', 'inscricao_url' );
-        $inscricao_inicio = cmb2_get_option( 'programacao_options', 'inscricao_inicio' );
-        $inscricao_fim = cmb2_get_option( 'programacao_options', 'inscricao_fim' );
-
-        $matricula_url = cmb2_get_option( 'programacao_options', 'matricula_url' );
-        $matricula_inicio = cmb2_get_option( 'programacao_options', 'matricula_inicio' );
-        $matricula_fim = cmb2_get_option( 'programacao_options', 'matrio$matricula_fim' );
-
-        if ($inscricao_url && $now >= $inscricao_inicio && $now <= $inscricao_fim) {
-            $items .= '<li class="nav-item"><a class="nav-link" href="' . $inscricao_url . '">Inscri&ccedil;&otilde;es</a></li>';
-        }
-
-        if ($matricula_url && $now >= $matricula_inicio && $now <= $matricula_fim) {
-            $items .= '<li class="nav-item"><a class="nav-link" href="' . $matricula_url . '">Inscri&ccedil;&otilde;es</a></li>';
-        }
-
-        return $items;
-    } );
-?>
-
 <a href="#inicio-menu" id="inicio-menu" class="visually-hidden">In&iacute;cio do menu</a>
 
 <?php
