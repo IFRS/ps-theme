447920076ba923131d3d2f7d385cc075cdae4356
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-09-05 15:07:37 -0300

    Correção no nome da fonte para pré-carregar

--- inc/fonts.php
+++ inc/fonts.php
@@ -2,7 +2,7 @@
 add_action('wp_head', function() {
     $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/inter-v3-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/bricolage-grotesque-v1-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
     <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
     <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
     <noscript>
