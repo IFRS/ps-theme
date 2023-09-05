35075c44e1f73808ba36bc1b5d2be9fe43f17650
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-09-05 14:25:06 -0300

    Nova marcação para informações dos avisos

--- partials/home/avisos.php
+++ partials/home/avisos.php
@@ -13,17 +13,15 @@
             <?php while ($noticias->have_posts()) : $noticias->the_post(); ?>
                 <article class="home-aviso">
                     <p class="home-aviso__meta">
-                        <span class="home-aviso__day"><?php echo get_the_date('d'); ?></span>
-                        <span class="home-aviso__month"><?php echo get_the_date('M'); ?></span>
+                        <?php echo get_the_date('d \d\e F'); ?>
+                        &ndash;
                     </p>
-                    <div class="home-aviso__content">
-                        <?php echo get_the_category_list(); ?>
-                        <h3 class="home-aviso__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    </div>
+                    <?php echo get_the_category_list(); ?>
+                    <h3 class="home-aviso__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 </article>
             <?php endwhile; ?>
         </div>
-        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="btn btn-primary text-uppercase">Todos os avisos</a>
+        <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>" class="btn btn-ps text-uppercase">Todos os avisos</a>
     <?php else : ?>
         <div class="alert alert-warning" role="alert">
             <p><strong>Ops!</strong> Ainda n&atilde;o existem avisos cadastrados.</p>
