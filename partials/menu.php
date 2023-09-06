021fa717dbd520a50e8dc21a3223149432a76111
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2023-09-06 11:18:06 -0300

    Ajustes no menu para habilitar o collapse

--- partials/menu.php
+++ partials/menu.php
@@ -1,16 +1,27 @@
 <a href="#inicio-menu" id="inicio-menu" class="visually-hidden">In&iacute;cio do menu</a>
 
-<?php
-    wp_nav_menu( array(
-        'menu_class'           => 'menu-principal navbar-nav flex-wrap mx-auto mb-2 mb-lg-0',
-        'menu_id'              => '',
-        'container'            => 'nav',
-        'container_class'      => 'navbar navbar-expand navbar-light',
-        'container_id'         => '',
-        'container_aria_label' => 'Navegação Principal',
-        'depth'                => 2,
-        'theme_location'       => 'main',
-    ));
-?>
+<?php $collapse_ID = uniqid('collapse-'); ?>
+
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $collapse_ID; ?>" aria-controls="<?php echo $collapse_ID; ?>" aria-expanded="false" aria-label="Alternar Menu Principal">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <?php
+            wp_nav_menu( array(
+                'menu_class'           => 'menu-principal navbar-nav flex-wrap mx-auto mb-2 mb-lg-0',
+                'menu_id'              => '',
+                // 'container'            => 'nav',
+                // 'container_class'      => 'navbar navbar-expand-lg navbar-light',
+                'container'            => 'div',
+                'container_class'      => 'collapse navbar-collapse',
+                'container_id'         => $collapse_ID,
+                'container_aria_label' => 'Navegação Principal',
+                'depth'                => 2,
+                'theme_location'       => 'main',
+            ));
+        ?>
+    </div>
+</nav>
 
 <a href="#fim-menu" id="fim-menu" class="visually-hidden">Fim do menu</a>
