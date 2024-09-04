9f71143ec05a039952d2d0f7b6436b4134d43820
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-09-04 14:47:21 -0300

    Mudança da fonte padrão para Kumbh Sans

--- inc/fonts.php
+++ inc/fonts.php
@@ -2,11 +2,11 @@
 add_action('wp_head', function() {
     $fonts_file = esc_url(get_template_directory_uri()). '/css/fonts.css';
 ?>
-    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/bricolage-grotesque-v1-latin-regular.woff2" as="font" type="font/woff2" crossorigin="anonymous"/>
-    <link rel="preload" href="<?php echo $fonts_file; ?>" as="style"/>
-    <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'"/>
+    <link rel="preload" href="<?php echo esc_url(get_template_directory_uri()); ?>/fonts/kumbh-sans-v22-latin-regular.woff2" as="font" type="font/woff2">
+    <link rel="preload" href="<?php echo $fonts_file; ?>" as="style">
+    <link rel="stylesheet" href="<?php echo $fonts_file; ?>" media="print" onload="this.media='all'">
     <noscript>
-        <link rel="stylesheet" href="<?php echo $fonts_file; ?>"/>
+        <link rel="stylesheet" href="<?php echo $fonts_file; ?>">
     </noscript>
 <?php
 }, 0);
