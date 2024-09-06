097ec8aad8aef4cc034e8411f1ca9a0294cfd4c0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-09-06 11:59:44 -0300

    Remoção de marcação não mais utilizada

--- footer.php
+++ footer.php
@@ -33,11 +33,6 @@
                             <svg role="img" xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 576 512" fill="#ffffff" aria-hidden="true"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg>
                         </a>
                     </li>
-                    <!-- <li class="menu-social__item">
-                        <a href="https://www.linkedin.com/school/ifrs" class="menu-social__link menu-social__link--linkedin" aria-label="Página do IFRS no LinkedIn">
-                            <svg role="img" xmlns="http://www.w3.org/2000/svg" height="1.1em" viewBox="0 0 448 512" fill="#ffffff" aria-hidden="true"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg> -->
-                        <!-- </a>
-                    </li> -->
                 </ul>
             </div>
             <div class="col mb-3">
