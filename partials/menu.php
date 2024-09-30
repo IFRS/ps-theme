a98fd763057e6b3dca1358ead3faa57121bf2e67
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2024-09-30 15:09:58 -0300

    Menu principal sem collapse

--- partials/menu.php
+++ partials/menu.php
@@ -1,27 +1,20 @@
 <a href="#inicio-menu" id="inicio-menu" class="visually-hidden">In&iacute;cio do menu</a>
 
-<?php $collapse_ID = uniqid('collapse-'); ?>
-
-<nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo $collapse_ID; ?>" aria-controls="<?php echo $collapse_ID; ?>" aria-expanded="false" aria-label="Alternar Menu Principal">
-        <span class="navbar-toggler-icon"></span>&nbsp;Menu
-        </button>
-        <?php
-            wp_nav_menu( array(
-                'menu_class'           => 'menu-principal navbar-nav flex-wrap mx-auto mb-2 mb-lg-0',
-                'menu_id'              => '',
-                // 'container'            => 'nav',
-                // 'container_class'      => 'navbar navbar-expand-lg navbar-light',
-                'container'            => 'div',
-                'container_class'      => 'collapse navbar-collapse',
-                'container_id'         => $collapse_ID,
-                'container_aria_label' => 'Navegação Principal',
-                'depth'                => 2,
-                'theme_location'       => 'main',
-            ));
-        ?>
-    </div>
-</nav>
+<div class="menu-wrapper">
+    <?php
+        $menu_ID = uniqid('menu-');
+        $container_ID = uniqid('container-');
+        wp_nav_menu( array(
+            'menu_class'           => 'menu-principal',
+            'menu_id'              => $menu_ID,
+            'container'            => 'nav',
+            'container_class'      => 'container',
+            'container_id'         => $container_ID,
+            'container_aria_label' => 'Navegação Principal',
+            'depth'                => 2,
+            'theme_location'       => 'main',
+        ));
+    ?>
+</div>
 
 <a href="#fim-menu" id="fim-menu" class="visually-hidden">Fim do menu</a>
