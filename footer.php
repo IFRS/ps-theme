012b9084e9ee2811f4229e781c4c00a403030df0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-04-03 15:31:50 -0300

    Início dos ajustes para o novo PS

--- footer.php
+++ footer.php
@@ -4,65 +4,64 @@
 
 <!-- Rodapé -->
 <footer>
-    <div class="container">
-        <div class="row row-cols-1 row-cols-md-3 align-items-center">
-            <div class="col mb-3">
-                <a href="https://ifrs.edu.br/" data-bs-toggle="tooltip" data-bs-placement="top" title="Portal do IFRS" class="d-block">
-                    <img class="m-auto img-fluid" loading="lazy" src="<?php echo get_stylesheet_directory_uri(); ?>/img/ifrs.png" alt="Marca do IFRS"/>
-                </a>
-            </div>
-            <div class="col mb-3 text-center">
-                <ul class="menu-social">
-                    <li class="menu-social__item">
-                        <a href="https://www.facebook.com/IFRSOficial" class="menu-social__link menu-social__link--facebook" aria-label="Página do IFRS no Facebook">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
-                        </a>
-                    </li>
-                    <li class="menu-social__item">
-                        <a href="https://www.instagram.com/IFRSOficial" class="menu-social__link menu-social__link--instagram" aria-label="Perfil do IFRS no Instagram">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
-                        </a>
-                    </li>
-                    <li class="menu-social__item">
-                        <a href="https://www.twitter.com/IFRSOficial" class="menu-social__link menu-social__link--twitter" aria-label="Perfil do IFRS no X (antigo Twitter)">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
-                        </a>
-                    </li>
-                    <li class="menu-social__item">
-                        <a href="https://www.youtube.com/IFRSOficial" class="menu-social__link menu-social__link--youtube" aria-label="Canal do IFRS no Youtube">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col mb-3">
-                <?php !dynamic_sidebar('contato'); ?>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-md-3 align-items-center">
+      <div class="col mb-3">
+        <a href="https://ifrs.edu.br/" data-bs-toggle="tooltip" data-bs-placement="top" title="Portal do IFRS" class="d-block">
+          <img class="m-auto img-fluid" loading="lazy" src="<?php echo get_parent_theme_file_uri( '/img/ifrs.png' ); ?>" alt="Marca do IFRS"/>
+        </a>
+      </div>
+      <div class="col mb-3 text-center">
+        <ul class="menu-social">
+          <li class="menu-social__item">
+            <a href="https://www.facebook.com/IFRSOficial" class="menu-social__link menu-social__link--facebook" aria-label="Página do IFRS no Facebook">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+            </a>
+          </li>
+          <li class="menu-social__item">
+            <a href="https://www.instagram.com/IFRSOficial" class="menu-social__link menu-social__link--instagram" aria-label="Perfil do IFRS no Instagram">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+            </a>
+          </li>
+          <li class="menu-social__item">
+            <a href="https://www.twitter.com/IFRSOficial" class="menu-social__link menu-social__link--twitter" aria-label="Perfil do IFRS no X (antigo Twitter)">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+            </a>
+          </li>
+          <li class="menu-social__item">
+            <a href="https://www.youtube.com/IFRSOficial" class="menu-social__link menu-social__link--youtube" aria-label="Canal do IFRS no Youtube">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" height="1.1em" fill="#ffffff" aria-hidden="true"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col mb-3">
+        <?php !dynamic_sidebar('contato'); ?>
+      </div>
     </div>
+  </div>
+  <div class="creditos">
+    <div class="container">
+      <!-- Wordpress -->
+      <a href="http://br.wordpress.org/" target="_blank" rel="noopener" data-bs-toggle="tooltip" title="Desenvolvido com Wordpress">
+        <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-wordpress.png' )); ?>" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
+      </a>
+      &mdash;
+      <!-- Git -->
+      <a href="https://github.com/IFRS/ps-theme/" target="_blank" rel="noopener" data-bs-toggle="tooltip" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
+        <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-git.png' )); ?>" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
+      </a>
+      &mdash;
+      <!-- Creative Commons -->
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener license" data-bs-toggle="tooltip" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
+        <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-cc-by-nc-sa.png' )); ?>" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
+      </a>
+    </div>
+  </div>
 </footer>
-
-<div class="creditos">
-    <div class="container">
-        <!-- Wordpress -->
-        <a href="http://br.wordpress.org/" target="_blank" rel="noopener" data-bs-toggle="tooltip" title="Desenvolvido com Wordpress">
-            <img loading="lazy" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/creditos-wordpress.png" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)"/>
-            <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
-        </a>
-        &mdash;
-        <!-- Git -->
-        <a href="https://github.com/IFRS/ps-theme/" target="_blank" rel="noopener" data-bs-toggle="tooltip" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
-            <img loading="lazy" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/creditos-git.png" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)"/>
-            <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
-        </a>
-        &mdash;
-        <!-- Creative Commons -->
-        <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener license" data-bs-toggle="tooltip" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
-            <img loading="lazy" src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/creditos-cc-by-nc-sa.png" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)"/>
-            <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
-        </a>
-    </div>
-</div>
 
 <?php echo get_template_part('partials/vlibras'); ?>
 
--- header.php
+++ header.php
@@ -1,62 +1,62 @@
 <!doctype html>
 <html <?php language_attributes(); ?> prefix="og: http://ogp.me/ns#">
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Departamento de Comunicação do IFRS">
-    <meta name="keywords" content="ifrs, processo, seletivo, vestibular, ingresso, estudar">
-    <meta name="description" content="Site com informações sobre o Processo Seletivo de Estudantes do IFRS.">
-    <!-- Favicons -->
-    <?php if (!has_site_icon()) echo get_template_part('partials/favicons'); ?>
-    <!-- WP -->
-    <?php wp_head(); ?>
-    <?php echo get_template_part('partials/header-image'); ?>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="Departamento de Comunicação do IFRS">
+  <meta name="keywords" content="ifrs, processo, seletivo, vestibular, ingresso, estudar">
+  <meta name="description" content="Site com informações sobre o Processo Seletivo de Estudantes do IFRS.">
+  <!-- Favicons -->
+  <?php if (!has_site_icon()) echo get_template_part('partials/favicons'); ?>
+  <!-- WP -->
+  <?php wp_head(); ?>
+  <?php echo get_template_part('partials/header-image'); ?>
 </head>
 
 <?php
-    $extra_image = get_theme_mod('extra-header-image');
-    $extra_image_position = get_theme_mod('extra-header-image-position');
+  $extra_image = get_theme_mod('extra-header-image');
+  $extra_image_position = get_theme_mod('extra-header-image-position');
 ?>
 
 <body <?php body_class() ?>>
-    <a href="#main" class="visually-hidden">Pular para o conte&uacute;do</a>
+  <a href="#main" class="visually-hidden">Pular para o conte&uacute;do</a>
 
-    <?php wp_body_open(); ?>
+  <?php wp_body_open(); ?>
 
-    <!-- Cabeçalho -->
-    <header>
-        <h1 class="visually-hidden"><?php bloginfo('name'); ?></h1>
-        <section class="container header">
-            <div class="row align-items-center justify-content-center">
-                <div class="col-auto col-lg-6 col-xl-auto flex-shrink-1 px-5 p-sm-0">
-                    <?php the_custom_logo(); ?>
-                </div>
-                <?php if ($extra_image) : ?>
-                    <?php list($width, $height, $type, $attr) = getimagesize($extra_image); ?>
-                    <div class="col-lg-6 col-xl-auto flex-shrink-1 d-none d-lg-block <?php echo ($extra_image_position ? 'order-first' : 'order-last') ?>">
-                        <img src="<?php echo esc_url($extra_image); ?>" aria-hidden="true" alt="" loading="lazy" class="extra-header-image" <?php echo $attr; ?>>
-                    </div>
-                <?php endif; ?>
-            </div>
-            <?php if (display_header_text()) : ?>
-                <p><?php bloginfo('description'); ?></p>
-            <?php endif; ?>
+  <!-- Cabeçalho -->
+  <header>
+    <h1 class="visually-hidden"><?php bloginfo('name'); ?></h1>
+    <section class="container header">
+      <div class="row align-items-center justify-content-center">
+        <div class="col-auto col-lg-6 col-xl-auto flex-shrink-1 px-5 p-sm-0">
+          <?php the_custom_logo(); ?>
+        </div>
+        <?php if ($extra_image) : ?>
+          <?php list($width, $height, $type, $attr) = getimagesize($extra_image); ?>
+          <div class="col-lg-6 col-xl-auto flex-shrink-1 d-none d-lg-block <?php echo ($extra_image_position ? 'order-first' : 'order-last') ?>">
+            <img src="<?php echo esc_url($extra_image); ?>" aria-hidden="true" alt="" loading="lazy" class="extra-header-image" <?php echo $attr; ?>>
+          </div>
+        <?php endif; ?>
+      </div>
+      <?php if (display_header_text()) : ?>
+        <p><?php bloginfo('description'); ?></p>
+      <?php endif; ?>
 
-        </section>
-    </header>
+    </section>
 
     <!-- Menu -->
     <?php echo get_template_part('partials/menu'); ?>
+  </header>
 
-    <!-- Corpo -->
-    <a href="#inicio-conteudo" id="inicio-conteudo" class="visually-hidden">In&iacute;cio do conte&uacute;do</a>
+  <!-- Corpo -->
+  <a href="#inicio-conteudo" id="inicio-conteudo" class="visually-hidden">In&iacute;cio do conte&uacute;do</a>
 
-    <main role="main" id="main">
-    <?php
-        if ( function_exists('yoast_breadcrumb') && !is_front_page() ) {
-            yoast_breadcrumb( '<div class="breadcrumb-wrapper"><nav aria-label="Você está em:" class="container">', '</nav></div>' );
-        } else {
-            ps_breadcrumb();
-        }
-    ?>
+  <main role="main" id="main">
+  <?php
+    if ( function_exists('yoast_breadcrumb') && !is_front_page() ) {
+      yoast_breadcrumb( '<div class="breadcrumb-wrapper"><nav aria-label="Você está em:" class="container">', '</nav></div>' );
+    } else {
+      ps_breadcrumb();
+    }
+  ?>
