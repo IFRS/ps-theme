88bf177cf29c93fc051d99d09eea5b4b46a904a0
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2025-08-27 09:29:00 -0300

    Ajuste nos créditos

--- footer.php
+++ footer.php
@@ -47,13 +47,13 @@
         <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-wordpress.png' )); ?>" alt="Desenvolvido com Wordpress (abre uma nova p&aacute;gina)"/>
         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
       </a>
-      &mdash;
+      &nbsp;&mdash;&nbsp;
       <!-- Git -->
       <a href="https://github.com/IFRS/ps-theme/" target="_blank" rel="noopener" data-bs-toggle="tooltip" title="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3">
         <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-git.png' )); ?>" alt="C&oacute;digo-fonte deste tema sob a licen&ccedil;a GPLv3 (abre uma nova p&aacute;gina)"/>
         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" aria-hidden="true"><path d="M0 0v8h8v-2h-1v1h-6v-6h1v-1h-2zm4 0l1.5 1.5-2.5 2.5 1 1 2.5-2.5 1.5 1.5v-4h-4z" /></svg>
       </a>
-      &mdash;
+      &nbsp;&mdash;&nbsp;
       <!-- Creative Commons -->
       <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank" rel="noopener license" data-bs-toggle="tooltip" title="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional">
         <img loading="lazy" src="<?php echo esc_url(get_parent_theme_file_uri( '/img/creditos-cc-by-nc-sa.png' )); ?>" alt="M&iacute;dia licenciada sob a Licen&ccedil;a Creative Commons Atribui&ccedil;&atilde;o-N&atilde;oComercial-CompartilhaIgual 4.0 Internacional (abre uma nova p&aacute;gina)"/>
