c987fac68267d3ef419cd0c1aa99c25adf9a0879
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-03-24 15:56:24 -0300

    Adição de pattern de ajuda para páginas

---
+++ patterns/ajuda.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Title: Ajuda
+ * Slug: ifrs-ps/ajuda
+ * Description: Ajuda para os usuários encontrarem o que precisam
+ * Categories: call-to-action, featured
+ * Keywords: ajuda, faq, perguntas frequentes, tutoriais
+ * Viewport Width: 1280
+ * Post Types: page
+ * Inserter: true
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"nowrap","verticalAlignment":"center","className":"ajuda"}} -->
+<div class="wp-block-group ajuda">
+  <!-- wp:heading {"textAlign":"center","style":{"typography":{"textTransform":"uppercase"}}} -->
+  <h2 class="wp-block-heading has-text-align-center" style="text-transform:uppercase">Você tem alguma dúvida?</h2>
+  <!-- /wp:heading -->
+
+  <!-- wp:search {"label":"Pesquisar","showLabel":false,"placeholder":"Buscar em todo o site","width":100,"widthUnit":"%","buttonText":"Pesquisar","buttonUseIcon":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+
+  <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+  <div class="wp-block-buttons">
+    <!-- wp:button {"textAlign":"center"} -->
+    <div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button">Perguntas Frequentes</a></div>
+    <!-- /wp:button -->
+
+    <!-- wp:button {"textAlign":"center"} -->
+    <div class="wp-block-button"><a class="wp-block-button__link has-text-align-center wp-element-button">Tutoriais</a></div>
+    <!-- /wp:button -->
+  </div>
+  <!-- /wp:buttons -->
+</div>
+<!-- /wp:group -->
