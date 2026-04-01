5f33464a6abe43b919eabdfef8f67657e0283cf6
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-04-01 17:32:29 -0300

    Ajuste no título do template home

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
   <?php ob_start(); ?>
 
   <!-- wp:heading {"className":"mb-4"} -->
-  <h2 class="wp-block-heading mb-4">Todas as Not&iacute;cias</h2>
+  <h2 class="wp-block-heading mb-4">Todos os Avisos</h2>
   <!-- /wp:heading -->
 
   <!-- wp:template-part {"slug":"avisos","lock":{"move":true,"remove":true}} /-->
