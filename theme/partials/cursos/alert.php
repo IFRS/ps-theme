458addf999c6cb22e7633c4b84ee50ab3efd7da7
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-04-08 11:25:15 -0300

    Ajuste no aviso para a listagem de Cursos

--- theme/partials/cursos/alert.php
+++ theme/partials/cursos/alert.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-12">
-        <div class="alert alert-warning" role="alert">
-            <p>Para saber mais sobre a forma de distribui&ccedil;&atilde;o das vagas, leia os <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>">editais</a>.</p>
+        <div class="alert alert-info" role="alert">
+            <p>Para saber mais sobre a forma de distribui&ccedil;&atilde;o das vagas, confira os <a href="<?php echo get_post_type_archive_link( 'edital' ); ?>">editais</a>.</p>
         </div>
     </div>
 </div>
