ebe178c57c52474288f2505532f7be0a86fd08c8
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-04-08 11:24:05 -0300

    Reorganização dos filtros para Cursos, mudança nos ícones e botões de ação

--- theme/partials/cursos/filter.php
+++ theme/partials/cursos/filter.php
@@ -1,68 +1,86 @@
 <?php
-    $taxonomies = array();
-    $taxonomies[] = get_taxonomy( 'campus' );
-    $taxonomies[] = get_taxonomy( 'modalidade' );
-    $taxonomies[] = get_taxonomy( 'turno' );
-    // $taxonomies[] = get_taxonomy( 'formaingresso' );
+  $taxonomies = array();
+  $taxonomies[] = get_taxonomy( 'campus' );
+  $taxonomies[] = get_taxonomy( 'modalidade' );
+  $taxonomies[] = get_taxonomy( 'turno' );
+  // $taxonomies[] = get_taxonomy( 'formaingresso' );
 ?>
-<form class="cursos__filters row g-2 align-items-center justify-content-start" method="POST" action="<?php echo esc_url(get_post_type_archive_link( 'curso' )); ?>">
+<form class="cursos__filters" method="POST" action="<?php echo esc_url(get_post_type_archive_link( 'curso' )); ?>">
+  <div class="row g-2 align-items-center justify-content-start">
     <?php foreach ($taxonomies as $taxonomy) : ?>
-        <div class="col-auto">
-            <?php $field_id = uniqid(); ?>
-            <label for="<?php echo $field_id; ?>" class="visually-hidden"><?php echo $taxonomy->labels->singular_name ?></label>
-            <?php
-                $selected = 0;
+    <div class="col-auto">
+      <?php $field_id = uniqid(); ?>
+      <label for="<?php echo $field_id; ?>" class="visually-hidden"><?php echo $taxonomy->labels->singular_name ?></label>
+      <?php
+        $selected = 0;
 
-                if (isset($_POST[$taxonomy->name]) && !is_array($_POST[$taxonomy->name])) {
-                    $candidate = sanitize_key(wp_unslash($_POST[$taxonomy->name]));
+        if (isset($_POST[$taxonomy->name]) && !is_array($_POST[$taxonomy->name])) {
+          $candidate = sanitize_key(wp_unslash($_POST[$taxonomy->name]));
 
-                    if (!empty($candidate) && term_exists($candidate, $taxonomy->name)) {
-                        $selected = $candidate;
-                    }
-                }
+          if (!empty($candidate) && term_exists($candidate, $taxonomy->name)) {
+            $selected = $candidate;
+          }
+        }
 
-                wp_dropdown_categories( array(
-                    'show_option_all' => $taxonomy->labels->all_items,
-                    'taxonomy'        => $taxonomy->name,
-                    'name'            => $taxonomy->name,
-                    'orderby'         => 'name',
-                    'value_field'     => 'slug',
-                    'selected'        => $selected,
-                    'hierarchical'    => true,
-                    'hide_empty'      => false,
-                    'id'              => $field_id,
-                    'class'           => 'form-select',
-                ) );
-            ?>
-        </div>
+        wp_dropdown_categories( array(
+          'show_option_all' => $taxonomy->labels->all_items,
+          'taxonomy'        => $taxonomy->name,
+          'name'            => $taxonomy->name,
+          'orderby'         => 'name',
+          'value_field'     => 'slug',
+          'selected'        => $selected,
+          'hierarchical'    => true,
+          'hide_empty'      => false,
+          'id'              => $field_id,
+          'class'           => 'form-select',
+        ) );
+        ?>
+    </div>
     <?php endforeach; ?>
 
-    <div class="col-auto">
+    <div class="col">
         <?php $field_id = uniqid(); ?>
         <label class="visually-hidden" for="<?php echo $field_id; ?>">Buscar por:</label>
-        <input class="form-control" type="text" value="<?php echo (get_search_query() ?? ''); ?>" name="s" id="<?php echo $field_id; ?>" placeholder="Buscar nos cursos..."/>
+        <input class="form-control" type="text" value="<?php echo (get_search_query() ?? ''); ?>" name="s" id="<?php echo $field_id; ?>" placeholder="Busque pelo curso..." style="min-width: 250px;"/>
     </div>
 
-    <div class="col-auto">
-        <button type="submit" class="btn btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Filtrar Cursos">
-            <span class="visually-hidden">Filtrar Cursos</span>
-            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 22.2 22.2" style="enable-background:new 0 0 22.2 22.2;" xml:space="preserve">
-                <g><path class="st0" d="M21.8,19.3l-4.6-4.6c1.1-1.6,1.7-3.4,1.7-5.3c0-1.3-0.2-2.5-0.7-3.7c-0.5-1.2-1.2-2.2-2-3
-                c-0.8-0.8-1.8-1.5-3-2C11.9,0.2,10.7,0,9.4,0C8.1,0,6.9,0.2,5.8,0.7c-1.2,0.5-2.2,1.2-3,2c-0.8,0.8-1.5,1.8-2,3
-                C0.2,6.9,0,8.1,0,9.4c0,1.3,0.2,2.5,0.7,3.7c0.5,1.2,1.2,2.2,2,3c0.8,0.8,1.8,1.5,3,2c1.2,0.5,2.4,0.7,3.7,0.7c2,0,3.7-0.6,5.3-1.7
-                l4.6,4.6c0.3,0.3,0.7,0.5,1.2,0.5c0.5,0,0.9-0.2,1.2-0.5c0.3-0.3,0.5-0.7,0.5-1.2C22.2,20.1,22.1,19.7,21.8,19.3z M13.6,13.6
-                c-1.2,1.2-2.6,1.8-4.2,1.8c-1.6,0-3.1-0.6-4.2-1.8C4,12.5,3.4,11.1,3.4,9.4c0-1.6,0.6-3.1,1.8-4.2C6.4,4,7.8,3.4,9.4,3.4
-                c1.6,0,3.1,0.6,4.2,1.8c1.2,1.2,1.8,2.6,1.8,4.2C15.4,11.1,14.8,12.5,13.6,13.6z"/></g>
-            </svg>
-        </button>
-        <a href="<?php echo get_post_type_archive_link( 'curso' ); ?>" class="btn btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" title="Limpar Filtros">
-            <span class="visually-hidden">Limpar Filtros</span>
-            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs">
-                <polyline points="3 6 5 6 21 6"></polyline>
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                <line x1="10" y1="11" x2="10" y2="17"></line>
-                <line x1="14" y1="11" x2="14" y2="17"></line>
-            </svg>
-        </a>
+    <div class="col-auto ms-auto">
+      <button type="submit" class="btn btn-sm btn-primary me-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Filtrar Cursos">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M4 4h16v2.172a2 2 0 0 1 -.586 1.414l-4.414 4.414v7l-6 2v-8.5l-4.48 -4.928a2 2 0 0 1 -.52 -1.345v-2.227z" />
+        </svg>
+        <span>Filtrar Cursos</span>
+      </button>
+      <a href="<?php echo get_post_type_archive_link( 'curso' ); ?>" class="btn btn-sm btn-outline-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="Limpar Filtros">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M4 7l16 0" />
+          <path d="M10 11l0 6" />
+          <path d="M14 11l0 6" />
+          <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+          <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+        </svg>
+        <span>Limpar Filtros</span>
+      </a>
     </div>
+  </div>
 </form>
