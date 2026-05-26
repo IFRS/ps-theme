3f9b80c88133b5d1d115f79ea7f08c7f6266190b
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-05-26 15:22:30 -0300

    Ajustes na área para resultados, unificação no template archive

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,12 +1,8 @@
 <?php get_header(); ?>
 
 <?php if (chamada_get_option('publish', false)) : ?>
-  <section class="home-chamadas">
-    <div class="container">
-      <div class="row">
-        <?php echo get_template_part('partials/home/chamadas'); ?>
-      </div>
-    </div>
+  <section class="container">
+    <?php echo get_template_part('partials/chamadas'); ?>
   </section>
 <?php endif; ?>
 
--- theme/partials/chamadas.php
+++ theme/partials/chamadas.php
@@ -2,9 +2,7 @@
   <h2 class="chamadas__title"><?php echo esc_html(chamada_get_option('title', __('Chamadas', 'ifrs-ps-theme'))); ?></h2>
   <?php echo wpautop(wp_kses_post(chamada_get_option('desc', '')), true); ?>
   <p>
-    <small>
-      <?php printf(__('Os resultados ser&atilde;o divulgados conforme <a href="%s">Cronograma</a>.', 'ifrs-ps-theme'), esc_url(get_post_type_archive_link('evento'))); ?>
-    </small>
+    <?php printf(__('Os resultados ser&atilde;o divulgados conforme <a href="%s">Cronograma</a>.', 'ifrs-ps-theme'), esc_url(get_post_type_archive_link('evento'))); ?>
   </p>
 
   <p v-if="!selectedCampus">Selecione seu Campus abaixo.</p>
@@ -39,6 +37,12 @@
           </a>
         </div>
       </div>
+    </div>
+    <div v-if="selectedCampus && Object.keys(chamadas).length === 0" class="alert alert-warning d-flex align-items-center text-start fw-medium" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+      </svg>
+      <div>Ainda n&atilde;o h&aacute; conte&uacute;do dispon&iacute;vel para este Campus.</div>
     </div>
   </div>
 </article>
