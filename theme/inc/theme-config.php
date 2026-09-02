641235a3c2d2ac5a6013f727d7f688da336c58b4
Ricardo Moro <ricardo.moro@ifrs.edu.br>
2026-09-02 16:35:38 -0300

    Ajustes e correções para melhor funcionamento da imagem de cabeçalho (fundo)

--- theme/inc/theme-config.php
+++ theme/inc/theme-config.php
@@ -59,9 +59,9 @@ add_theme_support('custom-logo', array(
 add_theme_support('custom-header', array(
   'default-image'          => '',
   'width'                  => 1920,
-  'height'                 => 480,
+  'height'                 => 300,
   'flex-height'            => true,
-  'flex-width'             => false,
+  'flex-width'             => true,
   'uploads'                => true,
   'random-default'         => false,
   'header-text'            => true,
